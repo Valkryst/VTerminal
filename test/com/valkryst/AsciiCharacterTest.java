9f5b321315efbfdfc4b73e7c4c3d9e5b580e3891
Valkryst <valkryst@valkryst.com>
2017-05-09 18:30:19 -0300

    Changes imports.

--- test/com/valkryst/AsciiCharacterTest.java
+++ test/com/valkryst/AsciiCharacterTest.java
@@ -1,9 +1,10 @@
 package com.valkryst;
 
 import com.valkryst.AsciiPanel.AsciiCharacter;
-import javafx.scene.paint.Color;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.awt.*;
 
 public class AsciiCharacterTest {
     @Test
