13990c4fed714e066fe0cdab869278b094d54254
Valkryst <valkryst@valkryst.com>
2017-06-18 14:53:05 -0300

    Removes unnecessary range check from flipCharactersVertically.

--- src/com/valkryst/VTerminal/AsciiString.java
+++ src/com/valkryst/VTerminal/AsciiString.java
@@ -718,10 +718,6 @@ public class AsciiString {
 
         rangeIndices.clampValuesToRange(0, characters.length);
 
-        if (isRangeValid(rangeIndices) == false) {
-            return;
-        }
-
         final int beginIndex = rangeIndices.getBegin();
         final int endIndex = rangeIndices.getEnd();
 
