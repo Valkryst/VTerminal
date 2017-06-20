9fe1731146227d404e10998ba1971f1fed3708d0
Valkryst <valkryst@valkryst.com>
2017-06-20 20:58:07 -0300

    Initial commit.

---
+++ src/com/valkryst/VTerminal/builder/component/ComponentBuilder.java
@@ -0,0 +1,76 @@
+package com.valkryst.VTerminal.builder.component;
+
+import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.component.Component;
+import lombok.Getter;
+
+public class ComponentBuilder<C extends Component, B extends ComponentBuilder<C, B>> {
+    /** The x-axis (column) coordinate of the top-left character. */
+    @Getter protected int columnIndex;
+    /** The y-axis (row) coordinate of the top-left character. */
+    @Getter protected int rowIndex;
+
+    /** The panel on which the button is to be placed. */
+    @Getter  protected Panel panel;
+
+    public ComponentBuilder() {
+        reset();
+    }
+
+    /**
+     * Uses the builder to construct a new component.
+     *
+     * @return
+     *         The new component.
+     *
+     * @throws IllegalStateException
+     *          If something is wrong with the builder's state.
+     */
+    public C build() {
+        return null;
+    }
+
+    /**
+     * Checks the current state of the builder.
+     *
+     * @throws IllegalStateException
+     *          If something is wrong with the builder's state.
+     */
+    protected void checkState() throws IllegalStateException {
+        if (panel == null) {
+            throw new IllegalStateException("The component must have a panel to be placed on.");
+        }
+    }
+
+    /** Resets the builder to it's default state. */
+    public void reset() {
+        columnIndex = 0;
+        rowIndex = 0;
+
+        panel = null;
+    }
+
+    public B setColumnIndex(final int columnIndex) {
+        if (columnIndex >= 0) {
+            this.columnIndex = columnIndex;
+        }
+
+        return (B)this;
+    }
+
+    public B setRowIndex(final int rowIndex) {
+        if (rowIndex >= 0) {
+            this.rowIndex = rowIndex;
+        }
+
+        return (B)this;
+    }
+
+    public B setPanel(final Panel panel) {
+        if (panel != null) {
+            this.panel = panel;
+        }
+
+        return (B)this;
+    }
+}
