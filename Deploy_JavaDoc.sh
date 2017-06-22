2b5387ebe1c951fee59cc5f1a64a33528a51c312
Valkryst <Valkryst@users.noreply.github.com>
2017-06-22 12:57:01 -0300

    Changes commit structure.

--- Deploy_JavaDoc.sh
+++ Deploy_JavaDoc.sh
@@ -30,13 +30,13 @@ if [ "$TRAVIS_REPO_SLUG" == "Valkryst/VTerminal" ] &&
     cp -R /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ $HOME/docs
 
     cd $HOME
-    git config --global user.name "Valkryst"
-    git config --global user.email "valkryst@valkryst.com"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Valkryst/VTerminal gh-pages > /dev/null
 
     cd gh-pages
-    git rm -rf ./docs
-    cp -Rf /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ ./docs
+    git config user.name "Valkryst"
+    git config user.email "valkryst@valkryst.com"
+    git rm -rf *
+    cp -Rf /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ ./
     git add -f .
     git commit -m "Updates JavaDoc on successful Travis CI build. Build #$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages."
     git push -fq origin gh-pages > /dev/null
