673501421c51ad3486641ad3005d2db3557d1d59
Valkryst <Valkryst@users.noreply.github.com>
2017-06-22 13:36:11 -0300

    Sets second copy to be recursive.

--- Deploy_JavaDoc.sh
+++ Deploy_JavaDoc.sh
@@ -35,7 +35,7 @@ if [ "$TRAVIS_REPO_SLUG" == "Valkryst/VTerminal" ] &&
     git config user.name "Valkryst"
     git config user.email "valkryst@valkryst.com"
     cp /home/travis/build/Valkryst/VTerminal/target/site/apidocs/* .
-    cp /home/travis/build/Valkryst/VTerminal/target/site/apidocs/com/ .
+    cp -r /home/travis/build/Valkryst/VTerminal/target/site/apidocs/com/ .
     git add -f .
     git commit -m "Updates JavaDoc on successful Travis CI build. Build #$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages."
     git push -fq origin gh-pages > /dev/null
