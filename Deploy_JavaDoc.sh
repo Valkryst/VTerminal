735e6f6d26688280589bb536cd89fcfe7508f0c0
Valkryst <Valkryst@users.noreply.github.com>
2017-06-22 12:07:52 -0300

    Replaces canBuild variable with multi-part if-statement.

--- Deploy_JavaDoc.sh
+++ Deploy_JavaDoc.sh
@@ -20,27 +20,26 @@ echo -e "Branch:"
 echo -e "\tExpected:false"
 echo -e "\tActual:$TRAVIS_BRANCH\n"
 
-canBuild=$(expr "$TRAVIS_REPO_SLUG" = "Valkryst/VTerminal")
-canBuild=$(expr $(expr "$TRAVIS_JDK_VERSION" = "oraclejdk8") & "$canBuild")
-canBuild=$(expr $(expr "$TRAVIS_PULL_REQUEST" = "false") & "$canBuild")
-canBuild=$(expr $(expr "$TRAVIS_BRANCH" = "master") & "$canBuild")
+if ["$TRAVIS_REPO_SLUG" == "Valkryst/VTerminal"] &&
+   ["$TRAVIS_JDK_VERSION" == "oraclejdk8"] &&
+   ["$TRAVIS_PULL_REQUEST" == "false"] &&
+   ["$TRAVIS_BRANCH" == "master"]; then
+   
+    echo -e "Publishing JavaDoc...\n"
 
-if ["$canBuild" == "1"]; then
-  echo -e "Publishing JavaDoc...\n"
+    cp -R /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ $HOME/javadoc-latest
 
-  cp -R /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ $HOME/javadoc-latest
+    cd $HOME
+    git config --global user.name "Valkryst"
+    git config --global user.email "valkryst@valkryst.com"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Valkryst/VTerminal gh-pages > /dev/null
 
-  cd $HOME
-  git config --global user.name "Valkryst"
-  git config --global user.email "valkryst@valkryst.com"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Valkryst/VTerminal gh-pages > /dev/null
+    cd gh-pages
+    git rm -rf ./javadoc
+    cp -Rf /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ ./javadoc
+    git add -f .
+    git commit -m "Updates JavaDoc on successful Travis CI build. Build #$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages."
+    git push -fq origin gh-pages > /dev/null
 
-  cd gh-pages
-  git rm -rf ./javadoc
-  cp -Rf /home/travis/build/Valkryst/VTerminal/target/site/apidocs/ ./javadoc
-  git add -f .
-  git commit -m "Updates JavaDoc on successful Travis CI build. Build #$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages."
-  git push -fq origin gh-pages > /dev/null
-
-  echo -e "Published JavaDoc to gh-pages.\n"
+    echo -e "Published JavaDoc to gh-pages.\n"
 fi
