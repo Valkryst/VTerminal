327b6e952d06f58e0c4afadc07c58efcc8f851cf
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-06-30 10:05:13 -0300

    Fixes issue with the isPositionValid function.

--- src/com/valkryst/VTerminal/component/Component.java
+++ src/com/valkryst/VTerminal/component/Component.java
@@ -202,11 +202,11 @@ public class Component {
      *         Whether or not the specified position is within the bounds of the component.
      */
     public boolean isPositionValid(final int columnIndex, final int rowIndex) {
-        if (rowIndex < 0 || rowIndex > boundingBox.getHeight()) {
+        if (rowIndex < 0 || rowIndex > boundingBox.getHeight() - 1) {
             return false;
         }
 
-        if (columnIndex < 0 || columnIndex > boundingBox.getWidth()) {
+        if (columnIndex < 0 || columnIndex > boundingBox.getWidth() - 1) {
             return false;
         }
 
