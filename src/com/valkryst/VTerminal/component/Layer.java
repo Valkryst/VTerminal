310f06e6dbeb5c9807c9c7e20850ce441ad900ee
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-07 09:34:56 -0300

    Fixes constructor JavaDoc and adds draw functions.

--- src/com/valkryst/VTerminal/component/Layer.java
+++ src/com/valkryst/VTerminal/component/Layer.java
@@ -1,8 +1,12 @@
 package com.valkryst.VTerminal.component;
 
+import com.valkryst.VTerminal.font.Font;
+
+import java.awt.*;
+
 public class Layer extends Component {
     /**
-     * Constructs a new AsciiComponent.
+     * Constructs a new Layer.
      *
      * @param columnIndex
      *        The x-axis (column) coordinate of the top-left character.
@@ -18,5 +22,26 @@ public class Layer extends Component {
      */
     public Layer(int columnIndex, int rowIndex, int width, int height) {
         super(columnIndex, rowIndex, width, height);
+    }
+
+    @Override
+    public void draw(final Screen screen) {
+        throw new UnsupportedOperationException("A Layer must be drawn using the draw(canvas, font) method.");
+    }
+
+    /**
+     * Draws the layer onto the specified canvas using the specified font.
+     *
+     * @param gc
+     *         The graphics context to draw with.
+     *
+     * @param font
+     *         The font to draw with.
+     */
+    public void draw(final Graphics2D gc, final Font font) {
+        // Draw the screen onto the canvas:
+        for (int row = 0 ; row < height ; row++) {
+            strings[row].draw(gc, font, row);
+        }
     }
 }
