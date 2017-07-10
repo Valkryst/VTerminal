a3ea32512c369c89207d8ca347aa84ba596861aa
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-10 10:38:24 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/builder/component/LabelBuilderTest.java
@@ -0,0 +1,68 @@
+package com.valkryst.VTerminal.builder.component;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+public class LabelBuilderTest {
+    private LabelBuilder builder;
+
+    @Before
+    public void initalizeBuilder() {
+        builder = new LabelBuilder();
+    }
+
+    @Test
+    public void testReset() {
+        builder.setText("Testing");
+        builder.setBackgroundColor(Color.RED);
+        builder.setForegroundColor(Color.RED);
+
+        builder.reset();
+
+        Assert.assertEquals("", builder.getText());
+        Assert.assertEquals(new Color(0x366C9F), builder.getBackgroundColor());
+        Assert.assertEquals(new Color(0xFFCF0F), builder.getForegroundColor());
+    }
+
+    @Test
+    public void setText_withValidText() {
+        builder.setText("Testing");
+        Assert.assertEquals("Testing", builder.getText());
+    }
+
+    @Test
+    public void setText_withNullText() {
+        builder.setText("Testing");
+        builder.setText(null);
+        Assert.assertEquals("Testing", builder.getText());
+    }
+
+    @Test
+    public void setBackgroundColor_withValidColor() {
+        builder.setBackgroundColor(Color.RED);
+        Assert.assertEquals(Color.RED, builder.getBackgroundColor());
+    }
+
+    @Test
+    public void setBackgroundColor_checked_withNullColor() {
+        builder.setBackgroundColor(Color.RED);
+        builder.setBackgroundColor(null);
+        Assert.assertEquals(Color.RED, builder.getBackgroundColor());
+    }
+
+    @Test
+    public void setForegroundColor_checked_withValidColor() {
+        builder.setForegroundColor(Color.RED);
+        Assert.assertEquals(Color.RED, builder.getForegroundColor());
+    }
+
+    @Test
+    public void setForegroundColor_checked_withNullColor() {
+        builder.setForegroundColor(Color.RED);
+        builder.setForegroundColor(null);
+        Assert.assertEquals(Color.RED, builder.getForegroundColor());
+    }
+}
