f427a3ba0b0e820481f9937f4a808560724581b6
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-10 12:59:14 -0300

    Adds connector characters.

--- src/com/valkryst/VTerminal/printer/RectangleType.java
+++ src/com/valkryst/VTerminal/printer/RectangleType.java
@@ -3,11 +3,11 @@ package com.valkryst.VTerminal.printer;
 import lombok.Getter;
 
 public enum RectangleType {
-    SIMPLE(new char[]{'+', '+', '+', '+', '|', '-'}),
-    THIN(new char[]{'┌', '┐', '└', '┘', '│', '─'}),
-    HEAVY(new char[]{'╔', '╗', '╚', '╝', '║', '═'}),
-    MIXED_HEAVY_HORIZONTAL(new char[]{'╒', '╕', '╘', '╛', '│', '═'}),
-    MIXED_HEAVY_VERTICAL(new char[]{'╓', '╖', '╙', '╜', '║', '─'});
+    SIMPLE(new char[]{'+', '+', '+', '+', '|', '-', '+', '+', '+', '+', '+'}),
+    THIN(new char[]{'┌', '┐', '└', '┘', '│', '─', '┼', '┤', '├', '┬', '┴'}),
+    HEAVY(new char[]{'╔', '╗', '╚', '╝', '║', '═', '╬', '╣', '╠', '╦', '╩'}),
+    MIXED_HEAVY_HORIZONTAL(new char[]{'╒', '╕', '╘', '╛', '│', '═', '╪', '╡', '╞', '╤', '╧'}),
+    MIXED_HEAVY_VERTICAL(new char[]{'╓', '╖', '╙', '╜', '║', '─', '╫', '╢', '╟', '╥', '╨'});
 
     /** The top left character of a rectangle. */
     @Getter private final char topLeft;
@@ -21,6 +21,16 @@ public enum RectangleType {
     @Getter private final char vertical;
     /** The horizontal character of a rectangle. */
     @Getter private final char horizontal;
+    /** The cross connector character of a rectangle. */
+    @Getter private final char connectorCross;
+    /** The left connector character of a rectangle. */
+    @Getter private final char connectorLeft;
+    /** The right connector character of a rectangle. */
+    @Getter private final char connectorRight;
+    /** The downwards connector character of a rectangle. */
+    @Getter private final char connnectorDown;
+    /** The upwards connector character of a rectangle. */
+    @Getter private final char connectorUp;
 
     /**
      * Constructs a new RectangleType.
@@ -30,7 +40,7 @@ public enum RectangleType {
      */
     RectangleType(char[] boxCharacters) {
         if (boxCharacters.length != 6) {
-            boxCharacters = new char[]{'?', '?', '?', '?', '?', '?'};
+            boxCharacters = new char[]{'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '"'};
         }
 
         topLeft = boxCharacters[0];
@@ -39,5 +49,10 @@ public enum RectangleType {
         bottomRight = boxCharacters[3];
         vertical = boxCharacters[4];
         horizontal = boxCharacters[5];
+        connectorCross = boxCharacters[6];
+        connectorLeft = boxCharacters[7];
+        connectorRight = boxCharacters[8];
+        connnectorDown = boxCharacters[9];
+        connectorUp = boxCharacters[10];
     }
 }
