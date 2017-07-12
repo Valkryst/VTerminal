222466af6724ab7b9e81dac42de90243615befa4
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-12 12:28:17 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/font/FontTest.java
@@ -0,0 +1,32 @@
+package com.valkryst.VTerminal.font;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class FontTest {
+    private final Font font;
+
+    public FontTest() throws IOException, URISyntaxException {
+        font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/20pt/bitmap.png", "Fonts/DejaVu Sans Mono/20pt/data.fnt", 1);
+    }
+
+    @Test
+    public void testGetCharacterImage_withAllSymbolAlphabeticAndNumberAsciiCharacters() {
+        for (byte i = 33 ; i < 127 ; i++) {
+            Assert.assertNotNull(font.getCharacterImage((char) i));
+        }
+    }
+
+    @Test
+    public void testGetWidth() {
+        Assert.assertEquals(12, font.getWidth());
+    }
+
+    @Test
+    public void testGetHeight() {
+        Assert.assertEquals(24, font.getHeight());
+    }
+}
