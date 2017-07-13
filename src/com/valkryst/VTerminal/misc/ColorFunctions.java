89813d53c453337cd8b51fe3caabd64a50e7b960
Valkryst <valkryst@valkryst.com>
2017-07-13 11:47:49 -0300

    Initial commit.

---
+++ src/com/valkryst/VTerminal/misc/ColorFunctions.java
@@ -0,0 +1,25 @@
+package com.valkryst.VTerminal.misc;
+
+import java.awt.Color;
+import java.awt.Transparency;
+
+public class ColorFunctions {
+    /**
+     * Ensures that a color supports transparency.
+     *
+     * @param color
+     *         The color.
+     *
+     * @return
+     *         Either the input color, if it supports transparency,
+     *         or a new copy of the input color which supports
+     *         transparency.
+     */
+    public static Color enforceTransparentColor(final Color color) {
+        if (color.getTransparency() != Transparency.TRANSLUCENT) {
+            return new Color(color.getRGB(), true);
+        }
+
+        return color;
+    }
+}
