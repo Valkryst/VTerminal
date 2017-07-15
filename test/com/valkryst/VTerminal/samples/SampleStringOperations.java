f1b34b11437bac536acc3a84c1054ecf9a365ce1
Valkryst <valkryst@valkryst.com>
2017-07-15 11:06:03 -0300

    Adds more tests.

--- test/com/valkryst/VTerminal/samples/SampleStringOperations.java
+++ test/com/valkryst/VTerminal/samples/SampleStringOperations.java
@@ -19,12 +19,7 @@ public class SampleStringOperations {
 
         Thread.sleep(100);
 
-        screen.clear('X');
-
         screen.write("Writing a String on Line #2", 0, 1);
-
-        screen.write("Applying a Color Gradient on Line #3", 0, 2);
-        screen.getString(2).applyShadeGradient(Color.CYAN, true);
 
         screen.write("Horizontally Flipping a String on Line #4", 0, 3);
         screen.getString(3).flipCharactersHorizontally();
@@ -34,6 +29,34 @@ public class SampleStringOperations {
 
         screen.write("Vertically & Horizontally Flipping a String on Line #6", 0, 5);
         screen.getString(5).flipCharactersHorizontallyAndVertically();
+
+        screen.write("Applying a Tint Gradient on Line #7", 0, 6);
+        screen.getString(6).applyTintGradient(Color.CYAN, true);
+
+        screen.write("Applying a Shade Gradient on Line #8", 0, 7);
+        screen.getString(7).applyShadeGradient(Color.CYAN, true);
+
+        screen.write("Applying a Background Color Gradient on Line #9", 0, 8);
+        screen.getString(8).applyColorGradient(Color.CYAN, Color.PINK, true);
+
+        screen.write("Applying a Foreground Color Gradient on Line #10", 0, 9);
+        screen.getString(9).applyColorGradient(Color.CYAN, Color.PINK, false);
+
+        screen.write("Applying a Back/Foreground Color Gradient on Line #11", 0, 10);
+        screen.getString(10).applyColorGradient(Color.RED, Color.GREEN, false);
+        screen.getString(10).applyColorGradient(Color.CYAN, Color.PINK, true);
+
+        screen.write("Changing Background Color on Line #12", 0, 11);
+        screen.getString(11).setBackgroundColor(Color.RED);
+
+        screen.write("Changing Foreground Color on Line #13", 0, 12);
+        screen.getString(12).setForegroundColor(Color.RED);
+
+        screen.write("Changing Back/Foreground Color on Line #14", 0, 13);
+        screen.getString(13).setBackgroundAndForegroundColor(Color.RED, Color.BLUE);
+
+        screen.write("Applying Underline on Line #15", 0, 15);
+        screen.getString(15).underlineCharacters();
 
         panel.draw();
     }
