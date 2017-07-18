c4a6a8fa6f74fe54d48b8e49bfd7b7f0e3bf6601
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-18 14:33:16 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/samples/SampleDrawTime.java
@@ -0,0 +1,43 @@
+package com.valkryst.VTerminal.samples;
+
+import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VTerminal.font.Font;
+import com.valkryst.VTerminal.font.FontLoader;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class SampleDrawTime {
+    public static void main(final String[] args) throws IOException, URISyntaxException, InterruptedException {
+        final Font font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/18pt/bitmap.png", "Fonts/DejaVu Sans Mono/18pt/data.fnt", 1);
+        final Panel panel = new PanelBuilder().setFont(font).build();
+
+        int temp = 45;
+
+        long total = 1;
+        long counter = 1;
+
+        while(true) {
+            panel.getScreen().clear((char)temp);
+            panel.getScreen().setForegroundColor(new Color(255, 155, temp, 255));
+            panel.getScreen().setBackgroundColor(new Color(temp, 155, 255, 255));
+
+            temp++;
+
+            if (temp > 55) {
+                temp = 45;
+            }
+
+            final long bef = System.currentTimeMillis();
+            panel.draw();
+            final long res = System.currentTimeMillis() - bef;
+            counter++;
+            total += res;
+            System.out.println("Draw Took:\t" + res + "ms\t\tAvg Is:\t" + (total / counter) + "ms");
+
+            Thread.sleep(100);
+        }
+    }
+}
---
+++ test/com/valkryst/VTerminal/samples/SampleFPS.java
@@ -0,0 +1,42 @@
+package com.valkryst.VTerminal.samples;
+
+import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VTerminal.font.Font;
+import com.valkryst.VTerminal.font.FontLoader;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class SampleFPS {
+    public static void main(final String[] args) throws IOException, URISyntaxException, InterruptedException {
+        final Font font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/18pt/bitmap.png", "Fonts/DejaVu Sans Mono/18pt/data.fnt", 1);
+        final Panel panel = new PanelBuilder().setFont(font).build();
+
+        double smoothing = 0.9; // larger=more smoothing
+        double measurement = 0.0;
+
+        int temp = 45;
+
+        while(true) {
+            panel.getScreen().clear((char)temp);
+            panel.getScreen().setForegroundColor(new Color(255, 155, temp, 255));
+            panel.getScreen().setBackgroundColor(new Color(temp, 155, 255, 255));
+
+            temp++;
+
+            if (temp > 55) {
+                temp = 45;
+            }
+
+            final long bef = System.currentTimeMillis();
+            panel.draw();
+
+            final double current = 1000 / Math.max((System.currentTimeMillis() - bef), 1);
+            measurement = (measurement * smoothing) + (current * (1.0-smoothing));
+            System.out.println("Estimated FPS based on Past Render Times:\t" + measurement);
+            Thread.sleep(100);
+        }
+    }
+}
