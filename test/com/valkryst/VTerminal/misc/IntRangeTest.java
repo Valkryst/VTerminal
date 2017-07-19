beb523e22e87f796ab1e1812c7f31de7f7551a4f
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-19 11:21:13 -0300

    Removes clamp tests.

--- test/com/valkryst/VTerminal/misc/IntRangeTest.java
+++ test/com/valkryst/VTerminal/misc/IntRangeTest.java
@@ -33,11 +33,9 @@ public class IntRangeTest {
         Assert.assertEquals(-2, range.getEnd());
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConstructor_withBeginGreaterThanEnd() {
-        final IntRange range = new IntRange(2, 1);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(1, range.getEnd());
+        new IntRange(2, 1);
     }
 
     @Test
@@ -78,89 +76,5 @@ public class IntRangeTest {
     public void testHashCode_withNonEqualRanges() {
         final IntRange rangeB = new IntRange(6, 200);
         Assert.assertNotEquals(range.hashCode(), rangeB.hashCode());
-    }
-
-    @Test
-    public void testClampValuesToRange() {
-        range.clampValuesToRange(1, 50);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(50, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginLessThanClampBegin() {
-        range.clampValuesToRange(10, 200);
-        Assert.assertEquals(10, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginEqualToClampBegin() {
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginBetweenClampBeginAndEnd() {
-        final IntRange range = new IntRange(100, 200);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(100, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginEqualToClampEnd() {
-        range.clampValuesToRange(200, 200);
-        Assert.assertEquals(200, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginGreaterThanClampEnd() {
-        final IntRange range = new IntRange(201, 200);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(200, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withEndLessThanClampBegin() {
-        final IntRange range = new IntRange(1, -1);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(1, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withEndEqualToClampBegin() {
-        final IntRange range = new IntRange(1, 1);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(1, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withEndEqualToClampEnd() {
-        final IntRange range = new IntRange(200, 200);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(200, range.getBegin());
-        Assert.assertEquals(200, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withEndBetweenClampBeginAndEnd() {
-        final IntRange range = new IntRange(1, 100);
-        range.clampValuesToRange(1, 200);
-        Assert.assertEquals(1, range.getBegin());
-        Assert.assertEquals(100, range.getEnd());
-    }
-
-    @Test
-    public void testClampValuesToRange_withBeginGreaterThanEnd() {
-        final IntRange range = new IntRange(1, 100);
-        range.clampValuesToRange(5, 4);
-        Assert.assertEquals(4, range.getBegin());
-        Assert.assertEquals(4, range.getEnd());
     }
 }
