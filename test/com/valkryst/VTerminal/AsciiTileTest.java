a73a205addb0bca0f9d1d2665f9f7ef6eeb07724
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-02 09:20:29 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/AsciiTileTest.java
@@ -0,0 +1,47 @@
+package com.valkryst.VTerminal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.Color;
+
+public class AsciiTileTest {
+    @Test
+    public void testConstructor_withValidInput() {
+        final AsciiCharacter character = new AsciiCharacter('?');
+        final AsciiTile tile = new AsciiTile('?');
+        Assert.assertEquals(character, tile);
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testConstructor_withNullCharacter() {
+        new AsciiTile(null);
+    }
+
+    @Test
+    public void testConstructor_withValidCharacter() {
+        final AsciiCharacter character = new AsciiCharacter('a');
+        character.setHidden(true);
+        character.setBackgroundColor(Color.BLUE);
+        character.setForegroundColor(Color.RED);
+        character.getBoundingBox().setSize(66, 67);
+        character.getBoundingBox().setLocation(68, 69);
+        character.setUnderlined(true);
+        character.setUnderlineThickness(1);
+        character.setFlippedHorizontally(true);
+        character.setFlippedVertically(true);
+
+        final AsciiTile tile = new AsciiTile(character);
+        Assert.assertEquals(true, tile.isHidden());
+        Assert.assertEquals(Color.BLUE, tile.getBackgroundColor());
+        Assert.assertEquals(Color.RED, tile.getForegroundColor());
+        Assert.assertEquals(66, tile.getBoundingBox().width);
+        Assert.assertEquals(67, tile.getBoundingBox().height);
+        Assert.assertEquals(68, tile.getBoundingBox().x);
+        Assert.assertEquals(69, tile.getBoundingBox().y);
+        Assert.assertEquals(true, tile.isUnderlined());
+        Assert.assertEquals(1, tile.getUnderlineThickness());
+        Assert.assertEquals(true, tile.isFlippedHorizontally());
+        Assert.assertEquals(true, tile.isFlippedVertically());
+    }
+}
