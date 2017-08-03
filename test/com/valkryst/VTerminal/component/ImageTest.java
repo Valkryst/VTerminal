e1b0b207398859a035b98961777ac55cbafc5b96
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-03 11:14:26 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/component/ImageTest.java
@@ -0,0 +1,10 @@
+package com.valkryst.VTerminal.component;
+
+import org.junit.Test;
+
+public class ImageTest {
+    @Test
+    public void testConstructor() {
+        new Image(0, 0, 1, 1);
+    }
+}
