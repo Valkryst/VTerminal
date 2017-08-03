7119491f7e1b9f1bfd5fc0d2babd3302e015c080
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-03 12:03:25 -0300

    Uncomments tests and updates a few to match API changes.

--- test/com/valkryst/VTerminal/component/ButtonTest.java
+++ test/com/valkryst/VTerminal/component/ButtonTest.java
@@ -1,37 +1,19 @@
 package com.valkryst.VTerminal.component;
 
+import com.valkryst.VRadio.Radio;
+import com.valkryst.VTerminal.builder.component.ButtonBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.Color;
+
 public class ButtonTest {
-    /*
     private Button button;
 
     @Before
-    public void testInitalizeButton() {
-        button = new ButtonBuilder().setText("Testing")
-                .setRadio(new Radio<>())
-                .build();
-    }
-
-    @Test
-    public void testEnableBlinkEffect() {
-        button.enableBlinkEffect((short) 666);
-
-        for (final AsciiString string : button.getStrings()) {
-            for (final AsciiCharacterTest character : string.getCharacters()) {
-                Assert.assertEquals(666, character.getMillsBetweenBlinks());
-            }
-        }
-    }
-
-    @Test
-    public void testDisableBlinkEffect() {
-        button.enableBlinkEffect((short) 666);
-        button.disableBlinkEffect();
-
-        for (final AsciiString string : button.getStrings()) {
-            for (final AsciiCharacterTest character : string.getCharacters()) {
-                Assert.assertEquals(666, character.getMillsBetweenBlinks());
-            }
-        }
+    public void testInitializeButton() {
+        button = new ButtonBuilder().setText("Testing").setRadio(new Radio<>()).build();
     }
 
     @Test
@@ -53,97 +35,66 @@ public class ButtonTest {
     @Test
     public void testSetBackgroundColor_normal() {
         button.setBackgroundColor_normal(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getBackgroundColor_normal());
+        Assert.assertEquals(Color.RED, button.getBackgroundColor_normal());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetBackgroundColor_normal_withNullColor() {
-        final Color defaultColor = button.getBackgroundColor_normal();
-
         button.setBackgroundColor_normal(null);
-
-        Assert.assertNotEquals(defaultColor, button.getBackgroundColor_normal());
     }
 
     @Test
     public void testSetForegroundColor_normal() {
         button.setForegroundColor_normal(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getForegroundColor_normal());
+        Assert.assertEquals(Color.RED, button.getForegroundColor_normal());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetForegroundColor_normal_withNullColor() {
-        final Color defaultColor = button.getForegroundColor_normal();
-
         button.setForegroundColor_normal(null);
-
-        Assert.assertNotEquals(defaultColor, button.getForegroundColor_hover());
     }
 
     @Test
     public void testSetBackgroundColor_hover() {
         button.setBackgroundColor_hover(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getBackgroundColor_hover());
+        Assert.assertEquals(Color.RED, button.getBackgroundColor_hover());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetBackgroundColor_hover_withNullColor() {
-        final Color defaultColor = button.getBackgroundColor_hover();
-
         button.setBackgroundColor_hover(null);
-
-        Assert.assertNotEquals(defaultColor, button.getBackgroundColor_hover());
     }
 
     @Test
     public void testSetForegroundColor_hover() {
         button.setForegroundColor_hover(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getForegroundColor_hover());
+        Assert.assertEquals(Color.RED, button.getForegroundColor_hover());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetForegroundColor_hover_withNullColor() {
-        final Color defaultColor = button.getForegroundColor_hover();
-
         button.setForegroundColor_hover(null);
-
-        Assert.assertNotEquals(defaultColor, button.getForegroundColor_hover());
     }
 
     @Test
     public void testSetBackgroundColor_pressed() {
         button.setBackgroundColor_pressed(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getBackgroundColor_pressed());
+        Assert.assertEquals(Color.RED, button.getBackgroundColor_pressed());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetBackgroundColor_pressed_withNullColor() {
-        final Color defaultColor = button.getBackgroundColor_pressed();
-
         button.setBackgroundColor_pressed(null);
-
-        Assert.assertNotEquals(defaultColor, button.getBackgroundColor_pressed());
     }
 
     @Test
     public void testSetForegroundColor_pressed() {
-        button.setForegroundColor_hover(Color.RED);
-
-        Assert.assertNotEquals(Color.RED, button.getForegroundColor_pressed());
+        button.setForegroundColor_pressed(Color.RED);
+        Assert.assertEquals(Color.RED, button.getForegroundColor_pressed());
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void testSetForegroundColor_pressed_withNullColor() {
-        final Color defaultColor = button.getForegroundColor_pressed();
-
         button.setForegroundColor_pressed(null);
-
-        Assert.assertNotEquals(defaultColor, button.getForegroundColor_pressed());
     }
-    */
 }
