32432b17ccff388a45a1fd6bdc699fe89103f96f
Valkryst <valkryst@valkryst.com>
2017-08-04 10:45:41 -0300

    Updates reset function to reset radio. Updates to match API changes.

--- src/com/valkryst/VTerminal/builder/component/LabelBuilder.java
+++ src/com/valkryst/VTerminal/builder/component/LabelBuilder.java
@@ -2,24 +2,26 @@ package com.valkryst.VTerminal.builder.component;
 
 import com.valkryst.VTerminal.component.Label;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
 import java.awt.Color;
 
-public class LabelBuilder extends ComponentBuilder<Label, LabelBuilder> {
+public class LabelBuilder extends ComponentBuilder<Label> {
     /** The text to display on the label. */
-    @Getter private String text;
+    @Getter @Setter @NonNull private String text;
 
     /** The background color for when the label. */
-    @Getter private Color backgroundColor;
+    @Getter @Setter @NonNull private Color backgroundColor;
     /** The foreground color for when the label. */
-    @Getter private Color foregroundColor;
+    @Getter @Setter @NonNull private Color foregroundColor;
 
     @Override
     public Label build() {
         checkState();
 
         final Label label = new Label(this);
-        super.panel.addComponent(label);
+        super.getPanel().addComponent(label);
 
         return label;
     }
@@ -32,56 +34,5 @@ public class LabelBuilder extends ComponentBuilder<Label, LabelBuilder> {
 
         backgroundColor = new Color(0xFF366C9F, true);
         foregroundColor = new Color(0xFFFFCF0F, true);
-    }
-
-    /**
-     * Sets the text.
-     *
-     * @param text
-     *        The new text.
-     *
-     * @return
-     *        This.
-     */
-    public LabelBuilder setText(final String text) {
-        if (text != null) {
-            this.text = text;
-        }
-
-        return this;
-    }
-
-    /**
-     * Sets the background color.
-     *
-     * @param backgroundColor
-     *        The new background color.
-     *
-     * @return
-     *        This.
-     */
-    public LabelBuilder setBackgroundColor(final Color backgroundColor) {
-        if (backgroundColor != null) {
-            this.backgroundColor = backgroundColor;
-        }
-
-        return this;
-    }
-
-    /**
-     * Sets the foreground color.
-     *
-     * @param foregroundColor
-     *        The new foreground color.
-     *
-     * @return
-     *        This.
-     */
-    public LabelBuilder setForegroundColor(final Color foregroundColor) {
-        if (foregroundColor != null) {
-            this.foregroundColor = foregroundColor;
-        }
-
-        return this;
     }
 }
