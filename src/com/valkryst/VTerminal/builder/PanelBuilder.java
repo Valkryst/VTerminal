f0e33022d168ba34aaff24d485b87678bb4b0a3d
Valkryst <valkryst@valkryst.com>
2017-08-04 10:08:03 -0300

    Rewrites-away the setters.

--- src/com/valkryst/VTerminal/builder/PanelBuilder.java
+++ src/com/valkryst/VTerminal/builder/PanelBuilder.java
@@ -4,23 +4,24 @@ import com.valkryst.VTerminal.Panel;
 import com.valkryst.VTerminal.component.Screen;
 import com.valkryst.VTerminal.font.Font;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class PanelBuilder {
     /** The width of the panel, in characters. */
-    @Getter private int widthInCharacters = 80;
+    @Getter @Setter private int widthInCharacters = 80;
     /** The height of the panel, in characters. */
-    @Getter private int heightInCharacters = 24;
+    @Getter @Setter private int heightInCharacters = 24;
     /** The font to draw with. */
-    @Getter private Font font;
+    @Getter @Setter private Font font;
 
     /** The screen being displayed on the panel. */
-    @Getter private Screen screen;
+    @Getter @Setter private Screen screen;
 
     /** The frame in which the panel is to be placed. */
-    @Getter private JFrame frame;
+    @Getter @Setter private JFrame frame;
 
     /**
      * Uses the builder to construct a new VTerminal.
@@ -49,10 +50,21 @@ public class PanelBuilder {
     /**
      * Checks the current state of the builder.
      *
+     * @throws java.lang.IllegalArgumentException
+     *          If the width or height is less than one.
+     *
      * @throws NullPointerException
      *          If the font is null.
      */
     private void checkState() throws NullPointerException {
+        if (widthInCharacters < 1) {
+            throw new IllegalArgumentException("The width, in characters, cannot be less than one.");
+        }
+
+        if (heightInCharacters < 1) {
+            throw new IllegalArgumentException("The height, in characters, cannot be less than one.");
+        }
+
         if (font == null) {
             throw new NullPointerException("The panel must have an AsciiFont to draw with.");
         }
@@ -74,88 +86,5 @@ public class PanelBuilder {
         font = null;
         screen = null;
         frame = null;
-    }
-
-    /**
-     * Sets the frame.
-     *
-     * @param frame
-     *        The new frame.
-     *
-     * @return
-     *        This.
-     */
-    public PanelBuilder setJFrame(final JFrame frame) {
-        this.frame = frame;
-        return this;
-    }
-
-    /**
-     * Sets the width in characters.
-     *
-     * @param widthInCharacters
-     *        The new width in characters.
-     *
-     * @return
-     *        This.
-     */
-    public PanelBuilder setWidthInCharacters(final int widthInCharacters) {
-        if (widthInCharacters < 1) {
-            this.widthInCharacters = 1;
-        } else {
-            this.widthInCharacters = widthInCharacters;
-        }
-
-        return this;
-    }
-
-    /**
-     * Sets the height in characters.
-     *
-     * @param heightInCharacters
-     *        The new height in characters.
-     *
-     * @return
-     *        This.
-     */
-    public PanelBuilder setHeightInCharacters(final int heightInCharacters) {
-        if (heightInCharacters < 1) {
-            this.heightInCharacters = 1;
-        } else {
-            this.heightInCharacters = heightInCharacters;
-        }
-
-        return this;
-    }
-
-    /**
-     * Sets the font.
-     *
-     * @param asciiFont
-     *        The new font.
-     *
-     * @return
-     *        This.
-     */
-    public PanelBuilder setFont(final Font asciiFont) {
-        if (asciiFont != null) {
-            this.font = asciiFont;
-        }
-
-        return this;
-    }
-
-    /**
-     * Sets the screen.
-     *
-     * @param screen
-     *        The new screen.
-     *
-     * @return
-     *        This.
-     */
-    public PanelBuilder setScreen(final Screen screen) {
-        this.screen = screen;
-        return this;
     }
 }
