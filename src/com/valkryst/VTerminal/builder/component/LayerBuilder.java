ec9e96684ce8a3a240d95ad70877f134f51915a7
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-25 14:44:33 -0300

    Initial commit.

---
+++ src/com/valkryst/VTerminal/builder/component/LayerBuilder.java
@@ -0,0 +1,68 @@
+package com.valkryst.VTerminal.builder.component;
+
+import com.valkryst.VTerminal.component.Layer;
+import com.valkryst.VTerminal.misc.JSONFunctions;
+import lombok.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.awt.Color;
+
+@EqualsAndHashCode(callSuper=true)
+@ToString
+public class LayerBuilder extends ComponentBuilder<Layer> {
+    /** The width of the layer, in characters. */
+    @Getter @Setter private int width;
+    /** The height of the layer, in characters. */
+    @Getter @Setter private int height;
+
+    /** The foreground color. */
+    @Getter @Setter @NonNull private Color foregroundColor;
+    /** The background color. */
+    @Getter @Setter @NonNull private Color backgroundColor;
+
+    @Override
+    public Layer build() {
+        checkState();
+        return new Layer(this);
+    }
+
+    /** Resets the builder to it's default state. */
+    public void reset() {
+        super.reset();
+
+        width = 1;
+        height = 1;
+    }
+
+    @Override
+    public void parseJSON(final @NonNull JSONObject jsonObject) {
+        reset();
+        super.parseJSON(jsonObject);
+
+
+        final Integer width = JSONFunctions.getIntElement(jsonObject, "width");
+        final Integer height = JSONFunctions.getIntElement(jsonObject, "height");
+
+        final Color foregroundColor = super.loadColorFromJSON((JSONArray) jsonObject.get("foregroundColor"));
+        final Color backgroundColor = super.loadColorFromJSON((JSONArray) jsonObject.get("backgroundColor"));
+
+
+        if (width != null) {
+            this.width = width;
+        }
+
+        if (height != null) {
+            this.height = height;
+        }
+
+
+        if (foregroundColor != null) {
+            this.foregroundColor = foregroundColor;
+        }
+
+        if (backgroundColor != null) {
+            this.backgroundColor = backgroundColor;
+        }
+    }
+}
