16523480ffc9dc7d84204ec0f40db62723526303
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-25 10:12:20 -0300

    Adds support for loading components from JSON.

--- src/com/valkryst/VTerminal/builder/component/ComponentBuilder.java
+++ src/com/valkryst/VTerminal/builder/component/ComponentBuilder.java
@@ -2,7 +2,18 @@ package com.valkryst.VTerminal.builder.component;
 
 import com.valkryst.VRadio.Radio;
 import com.valkryst.VTerminal.component.Component;
+import com.valkryst.VTerminal.font.FontLoader;
 import lombok.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.awt.Color;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EqualsAndHashCode
 @ToString
@@ -59,5 +70,133 @@ public class ComponentBuilder<C extends Component> {
         rowIndex = 0;
 
         radio = null;
+    }
+
+    /**
+     * Loads a portion of the builder's data from JSON.
+     *
+     * @param jsonFilePath
+     *        The path to the JSON file.
+     *
+     * @throws FileNotFoundException
+     *         If the file does not exist, is a directory rather
+     *         than a regular file, or for some other reason cannot
+     *         be opened for reading.
+     *
+     * @throws ParseException
+     *         If there's an error when parsing the JSON.
+     */
+    public void loadFromJSON(final @NonNull String jsonFilePath) throws ParseException, FileNotFoundException {
+        if (jsonFilePath.isEmpty()) {
+            throw new IllegalArgumentException("The JSON file path cannot be empty.");
+        }
+
+        loadFromJSON(new FileInputStream(jsonFilePath));
+    }
+
+    /**
+     * Loads a portion of the builder's data from a JSON file
+     * within the Jar.
+     *
+     * @param jsonFilePath
+     *        The path to the JSON file.
+     *
+     * @throws ParseException
+     *         If there's an error when parsing the JSON.
+     */
+    public void loadFromJSONInJar(final @NonNull String jsonFilePath) throws ParseException {
+        if (jsonFilePath.isEmpty()) {
+            throw new IllegalArgumentException("The JSON file path cannot be empty.");
+        }
+
+        final ClassLoader classLoader = FontLoader.class.getClassLoader();
+        final InputStream jsonFileStream = classLoader.getResourceAsStream(jsonFilePath);
+
+        loadFromJSON(jsonFileStream);
+    }
+
+    /**
+     * Loads a portion of the builder's data from JSON.
+     *
+     * @param jsonFileStream
+     *        The JSON input stream.
+     *
+     * @throws ParseException
+     *         If there's an error when parsing the JSON.
+     */
+    public void loadFromJSON(final @NonNull InputStream jsonFileStream) throws ParseException {
+        // Load lines
+        final InputStreamReader isr = new InputStreamReader(jsonFileStream, StandardCharsets.UTF_8);
+        final BufferedReader br = new BufferedReader(isr);
+        final List<String> lines = br.lines().collect(Collectors.toList());
+
+        parseJSON(String.join("", lines));
+    }
+
+    /**
+     * Loads a portion of the builder's data from JSON.
+     *
+     * @param jsonData
+     *        The JSON.
+     *
+     * @throws ParseException
+     *         If there's an error when parsing the JSON.
+     */
+    public void parseJSON(final @NonNull String jsonData) throws ParseException {
+        final JSONParser parser = new JSONParser();
+        final Object object = parser.parse(jsonData);
+
+        parseJSON((JSONObject) object);
+    }
+
+    /**
+     * Loads a portion of the builder's data from JSON.
+     *
+     * @param jsonObject
+     *        The JSON.
+     */
+    public void parseJSON(final @NonNull JSONObject jsonObject) {
+        final Integer columnIndex = (int) (long) jsonObject.get("columnIndex");
+        final Integer rowIndex = (int) (long) jsonObject.get("rowIndex");
+
+        if (columnIndex != null) {
+            this.columnIndex = columnIndex;
+        }
+
+        if (rowIndex != null) {
+            this.rowIndex = rowIndex;
+        }
+    }
+
+    /**
+     * Loads a Color from a JSON array.
+     *
+     * The array is loaded as [red, green, blue] or a [red, green, blue, alpha].
+     * Any values after alpha are ignored.
+     *
+     * @param jsonArray
+     *        The array of color values.
+     *
+     * @return
+     *        The Color or null if the jsonArray is null.
+     *
+     * @throws IllegalStateException
+     *        If the array contains fewer than three values.
+     */
+    public Color loadColorFromJSON(final JSONArray jsonArray) {
+        if (jsonArray == null) {
+            return null;
+        }
+
+        if (jsonArray.size() >= 3) {
+            final Integer red = (int) (long) jsonArray.get(0);
+            final Integer green = (int) (long) jsonArray.get(1);
+            final Integer blue = (int) (long) jsonArray.get(2);
+            Integer alpha = jsonArray.size() >= 4 ? (int) (long) jsonArray.get(3) : 255;
+
+            return new Color(red, green, blue, alpha);
+        }
+
+        throw new IllegalStateException("Cannot load a color with fewer than 3 values.");
     }
 }
