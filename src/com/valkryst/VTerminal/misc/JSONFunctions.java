895a975c12300e72ee37139e8bb4d9f81f8bfa21
Valkryst <valkryst@valkryst.com>
2017-08-26 22:22:03 -0300

    Adds the loadColorFromJSON function.

--- src/com/valkryst/VTerminal/misc/JSONFunctions.java
+++ src/com/valkryst/VTerminal/misc/JSONFunctions.java
@@ -1,7 +1,10 @@
 package com.valkryst.VTerminal.misc;
 
 import lombok.NonNull;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import java.awt.Color;
 
 public class JSONFunctions {
     /**
@@ -25,5 +28,37 @@ public class JSONFunctions {
         } else {
             return (int) (long) object;
         }
+    }
+
+    /**
+     * Loads a Color from a JSON array.
+     *
+     * The array is loaded as [red, green, blue] or a [red, green, blue, alpha].
+     * Any values after alpha are ignored.
+     *
+     * @param jsonArray
+     *         The array of color values.
+     *
+     * @return
+     *         The Color or null if the jsonArray is null.
+     *
+     * @throws IllegalStateException
+     *         If the array contains fewer than three values.
+     */
+    public Color loadColorFromJSON(final JSONArray jsonArray) {
+        if (jsonArray == null) {
+            return null;
+        }
+
+        if (jsonArray.size() >= 3) {
+            final Integer red = (int) (long) jsonArray.get(0);
+            final Integer green = (int) (long) jsonArray.get(1);
+            final Integer blue = (int) (long) jsonArray.get(2);
+            Integer alpha = jsonArray.size() >= 4 ? (int) (long) jsonArray.get(3) : 255;
+
+            return new Color(red, green, blue, alpha);
+        }
+
+        throw new IllegalStateException("Cannot load a color with fewer than 3 values.");
     }
 }
