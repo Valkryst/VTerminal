459ed7fff56115c27fcff94e7385d5e6d0a2061e
Valkryst <valkryst@valkryst.com>
2017-08-26 22:46:25 -0300

    Removed a number of tests in expectation of large API changes.

---
+++ test/com/valkryst/VTerminal/samples/SampleScreenOnScreen.java
@@ -0,0 +1,45 @@
+package com.valkryst.VTerminal.samples;
+
+import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VTerminal.builder.component.ScreenBuilder;
+import com.valkryst.VTerminal.component.Screen;
+import com.valkryst.VTerminal.font.Font;
+import com.valkryst.VTerminal.font.FontLoader;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class SampleScreenOnScreen {
+    public static void main(final String[] args) throws IOException, URISyntaxException, InterruptedException, ParseException {
+        final Font font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/18pt/bitmap.png", "Fonts/DejaVu Sans Mono/18pt/data.fnt", 1);
+
+        final PanelBuilder builder = new PanelBuilder();
+        builder.setFont(font);
+
+        final Panel panel = builder.build();
+
+        Thread.sleep(100);
+
+        final ScreenBuilder screenBuilder = new ScreenBuilder();
+        screenBuilder.setColumnIndex(0);
+        screenBuilder.setRowIndex(0);
+        screenBuilder.setWidth(panel.getWidthInCharacters());
+        screenBuilder.setHeight(panel.getHeightInCharacters());
+        screenBuilder.setRadio(panel.getRadio());
+
+        final Screen screenA = screenBuilder.build();
+
+        screenBuilder.setColumnIndex(10);
+        screenBuilder.setRowIndex(10);
+        screenBuilder.setWidth(panel.getWidthInCharacters() - 10);
+        screenBuilder.setHeight(panel.getHeightInCharacters() - 10);
+        final Screen screenB = screenBuilder.build();
+
+        screenA.addComponent(screenB);
+        panel.swapScreen(screenA);
+
+        panel.draw();
+    }
+}
