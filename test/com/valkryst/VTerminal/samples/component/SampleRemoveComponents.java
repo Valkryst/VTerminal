5e3d5baf9c7acbc77b9748c0e5fc340badec31e1
Valkryst <valkryst@valkryst.com>
2017-10-05 11:40:45 -0300

    Initial commit.

---
+++ test/com/valkryst/VTerminal/samples/component/SampleRemoveComponents.java
@@ -0,0 +1,42 @@
+package com.valkryst.VTerminal.samples.component;
+
+import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VTerminal.builder.component.ScreenBuilder;
+import com.valkryst.VTerminal.component.Component;
+import com.valkryst.VTerminal.font.Font;
+import com.valkryst.VTerminal.font.FontLoader;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class SampleRemoveComponents {
+     public static void main (final String[] args) throws IOException, URISyntaxException, InterruptedException, ParseException {
+         final Font font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/18pt/bitmap.png", "Fonts/DejaVu Sans Mono/18pt/data.fnt", 1);
+
+         final PanelBuilder builder = new PanelBuilder();
+         builder.setFont(font);
+
+         final Panel panel = builder.build();
+
+         Thread.sleep(100);
+
+         final ScreenBuilder screenBuilder = new ScreenBuilder();
+         screenBuilder.loadFromJSON(System.getProperty("user.dir") + "/res_test/Sample Screen.json");
+         screenBuilder.setRadio(panel.getRadio());
+
+         panel.swapScreen(screenBuilder.build());
+
+         panel.draw();
+
+         // Remove all components
+         Thread.sleep(3000);
+
+         for (final Component component : panel.getScreen().getComponents()) {
+             panel.removeComponent(component);
+         }
+
+         panel.draw();
+     }
+}
