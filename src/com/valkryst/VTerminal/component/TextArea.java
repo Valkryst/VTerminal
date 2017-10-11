ec34c155809b69e8bba77319662c2e6dde9fc408
Valkryst <valkryst@valkryst.com>
2017-10-11 11:03:33 -0300

    Removes clearText calls from appendText function.

--- src/com/valkryst/VTerminal/component/TextArea.java
+++ src/com/valkryst/VTerminal/component/TextArea.java
@@ -697,11 +697,9 @@ public class TextArea extends Component {
         final AsciiString[] strings = super.getStrings();
 
         for (int i = 0 ; i < strings.length - 1 ; i++) {
-            clearText(i);
             setText(i, strings[i + 1]);
         }
 
-        clearText(strings.length - 1);
         setText(strings.length - 1, text);
 
         updateDisplayedCharacters();
