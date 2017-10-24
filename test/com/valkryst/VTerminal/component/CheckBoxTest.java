d6c21141d2f45a3c0e60dff29d52cb056522f936
Valkryst <valkryst@valkryst.com>
2017-10-24 10:47:26 -0300

    Adds tests for CheckBox.

---
+++ test/com/valkryst/VTerminal/component/CheckBoxTest.java
@@ -0,0 +1,69 @@
+package com.valkryst.VTerminal.component;
+
+import com.valkryst.VRadio.Radio;
+import com.valkryst.VTerminal.builder.component.CheckBoxBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckBoxTest {
+    private CheckBox checkBox;
+    private final Runnable function = () -> System.out.println("Test Func");
+
+    @Before
+    public void init() {
+        final CheckBoxBuilder builder = new CheckBoxBuilder();
+        builder.setText("Testing");
+        builder.setOnClickFunction(function);
+        builder.setRadio(new Radio<>());
+        checkBox = builder.build();
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testConstructor_withNullBuilder() {
+        new CheckBox(null);
+    }
+
+    @Test
+    public void testConstructor_withValidBuilder() {
+        final CheckBoxBuilder builder = new CheckBoxBuilder();
+        builder.setText("Testing");
+        builder.setOnClickFunction(function);
+        builder.setRadio(new Radio<>());
+        new CheckBox(builder);
+    }
+
+    @Test
+    public void testSetEmptyBoxCharacter() {
+        checkBox.setEmptyBoxCharacter('M');
+        Assert.assertEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+        checkBox.check();
+        Assert.assertNotEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+    }
+
+    @Test
+    public void testSetCheckedBoxCharacter() {
+        checkBox.setCheckedBoxCharacter('M');
+        Assert.assertNotEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+        checkBox.check();
+        Assert.assertEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+    }
+
+    @Test
+    public void testCheck() {
+        checkBox.setEmptyBoxCharacter('M');
+        Assert.assertEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+        checkBox.check();
+        Assert.assertNotEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+    }
+
+    @Test
+    public void testUncheck() {
+        checkBox.setEmptyBoxCharacter('M');
+        Assert.assertEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+        checkBox.check();
+        Assert.assertNotEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+        checkBox.uncheck();
+        Assert.assertEquals('M', checkBox.getString(0).getCharacters()[0].getCharacter());
+    }
+}
