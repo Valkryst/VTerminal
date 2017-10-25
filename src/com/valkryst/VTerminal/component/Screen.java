d7b855d11b118f56b2c7d0d3d78a1c0a40e3a168
Valkryst <valkryst@valkryst.com>
2017-10-25 10:38:52 -0300

    Removes debug output.

--- src/com/valkryst/VTerminal/component/Screen.java
+++ src/com/valkryst/VTerminal/component/Screen.java
@@ -513,10 +513,8 @@ public class Screen extends Component implements Receiver<String> {
 
         // Set up event listeners:
         component.createEventListeners(parentPanel);
-        System.out.println("Listeners created using " + (parentPanel == null) + " panel.");
 
         for (final EventListener eventListener : component.getEventListeners()) {
-            System.out.println("Added " + eventListener.getClass().getName() + " for " + component.getClass().getSimpleName());
             parentPanel.addListener(eventListener);
         }
     }
