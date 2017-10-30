5ea96f265f5b1c3ffec2e44daac4476a534f0949
Valkryst <valkryst@valkryst.com>
2017-10-30 14:14:35 -0300

    Removes class doc.

--- src/com/valkryst/VTerminal/shader/TextGlowShader.java
+++ src/com/valkryst/VTerminal/shader/TextGlowShader.java
@@ -7,9 +7,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-/**
- * The TextGlowShader cannot be combined with the TextShadowShader.
- */
 public class TextGlowShader implements Shader {
     @Override
     public BufferedImage run(final @NonNull BufferedImage image) {
