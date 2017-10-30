d56cc232f69313d70154c057de505af2acff4d6c
Valkryst <valkryst@valkryst.com>
2017-10-30 11:58:14 -0300

    Moves some helper code from TextShadowShader into Shader

--- src/com/valkryst/VTerminal/shader/Shader.java
+++ src/com/valkryst/VTerminal/shader/Shader.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
 
 public interface Shader {
     /**
@@ -50,5 +51,70 @@ public interface Shader {
         }
 
         return result;
+    }
+
+    /**
+     * Guesses the back/foreground colors of a character image.
+     *
+     * The most common color is assumed to be the background color while the second
+     * most common color is assumed to be the foreground color.
+     *
+     * This is not always correct.
+     *
+     * @param image
+     *          The character image.
+     *
+     * @return
+     *          An array containing the background and foreground colors.
+     *          The background color is [0] and the foreground color is [1].
+     *
+     * @throws java.lang.IllegalStateException
+     *          If there is only one color on the image.
+     *
+     *          If there is more than one color on the image.
+     */
+    default Color[] getBackgroundAndForegroundRGBColor(final @NonNull BufferedImage image) {
+        // Count occurrences of each color:
+        final HashMap<Integer, Integer> colorOccurrences = new HashMap<>();
+
+        for (int y = 0 ; y < image.getHeight() ; y++) {
+            for (int x = 0 ; x < image.getWidth() ; x++) {
+                int currentColor = image.getRGB(x, y);
+                int occurrences = colorOccurrences.getOrDefault(currentColor, 0);
+
+                colorOccurrences.put(currentColor, occurrences + 1);
+            }
+        }
+
+        // If there is only one color, then we're dealing with a blank AsciiCharacter.
+        // In this case, the shader does nothing.
+        if (colorOccurrences.size() == 1) {
+            throw new IllegalStateException("Cannot retrieve a character without it's background if there are less than two colors in the image.");
+        }
+
+        // If there are more than two colors, we're not dealing with an
+        // AsciiCharacter. In this case, the shader does nothing.
+        if (colorOccurrences.size() > 2) {
+            throw new IllegalStateException("Cannot retrieve a character without it's background if there are more than two colors in the image.");
+        }
+
+        // The most common color is probably the background color and the least
+        // common color is probably the foreground color.
+        // This isn't always going to be true, but for most characters it will be.
+        final Integer[] keySet = colorOccurrences.keySet().toArray(new Integer[colorOccurrences.size()]);
+        final Integer[] valueSet = colorOccurrences.values().toArray(new Integer[colorOccurrences.size()]);
+
+        final Color backgroundColor;
+        final Color foregroundColor;
+
+        if (valueSet[0] > valueSet[1]) {
+            backgroundColor = new Color(keySet[0]);
+            foregroundColor = new Color(keySet[1]);
+        } else {
+            backgroundColor = new Color(keySet[1]);
+            foregroundColor = new Color(keySet[0]);
+        }
+
+        return new Color[]{backgroundColor, foregroundColor};
     }
 }
--- src/com/valkryst/VTerminal/shader/TextShadowShader.java
+++ src/com/valkryst/VTerminal/shader/TextShadowShader.java
@@ -6,7 +6,6 @@ import lombok.NonNull;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.HashMap;
 
 @Data
 public class TextShadowShader implements Shader {
@@ -17,67 +16,26 @@ public class TextShadowShader implements Shader {
 
     @Override
     public BufferedImage run(final @NonNull BufferedImage image) {
-        // Count occurrences of each color:
-        final HashMap<Integer, Integer> colorOccurrences = new HashMap<>();
+        try {
+            final Color[] colors = getBackgroundAndForegroundRGBColor(image);
 
-        for (int y = 0 ; y < image.getHeight() ; y++) {
-            for (int x = 0 ; x < image.getWidth() ; x++) {
-                int currentColor = image.getRGB(x, y);
-                int occurrences = colorOccurrences.getOrDefault(currentColor, 0);
+            // Get the normal & background character images:
+            final BufferedImage normalChar = swapColor(image, colors[0], new Color(0, 0, 0, 0));
+            final BufferedImage shadowChar = swapColor(normalChar, colors[1], Color.BLACK);
 
-                colorOccurrences.put(currentColor, occurrences + 1);
-            }
-        }
+            // Combine images:
+            final BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            final Graphics2D gc = (Graphics2D) result.getGraphics();
 
-        // If there is only one color, then we're dealing with a blank AsciiCharacter.
-        // In this case, the shader does nothing.
-        if (colorOccurrences.size() == 1) {
+            gc.setColor(colors[0]);
+            gc.fillRect(0, 0, result.getWidth(), result.getHeight());
+            gc.drawImage(shadowChar, xOffset, yOffset, null);
+            gc.drawImage(normalChar, 0, 0, null);
+            gc.dispose();
+
+            return result;
+        } catch (final IllegalStateException e) {
             return image;
         }
-
-        // If there are more than two colors, we're not dealing with an
-        // AsciiCharacter. In this case, the shader does nothing.
-        if (colorOccurrences.size() > 2) {
-            return image;
-        }
-
-        // The most common color is probably the background color and the least
-        // common color is probably the foreground color.
-        // This isn't always going to be true, but for most characters it will be.
-        final Integer[] keySet = colorOccurrences.keySet().toArray(new Integer[colorOccurrences.size()]);
-        final Integer[] valueSet = colorOccurrences.values().toArray(new Integer[colorOccurrences.size()]);
-
-        final Color backgroundColor;
-        final Color foregroundColor;
-
-        if (valueSet[0] > valueSet[1]) {
-            backgroundColor = new Color(keySet[0]);
-            foregroundColor = new Color(keySet[1]);
-        } else {
-            backgroundColor = new Color(keySet[1]);
-            foregroundColor = new Color(keySet[0]);
-        }
-
-        // There's no character to add a shadow for, if the back/foreground colors
-        // are the same.
-        if (backgroundColor.equals(foregroundColor)) {
-            return image;
-        }
-
-        // Get the normal & background character images:
-        final BufferedImage normalChar = swapColor(image, backgroundColor, new Color(0, 0, 0, 0));
-        final BufferedImage shadowChar = swapColor(normalChar, foregroundColor, Color.BLACK);
-
-        // Combine images:
-        final BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        final Graphics2D gc = (Graphics2D) result.getGraphics();
-
-        gc.setColor(backgroundColor);
-        gc.fillRect(0, 0, result.getWidth(), result.getHeight());
-        gc.drawImage(shadowChar, xOffset, yOffset, null);
-        gc.drawImage(normalChar, 0, 0, null);
-        gc.dispose();
-
-        return result;
     }
 }
