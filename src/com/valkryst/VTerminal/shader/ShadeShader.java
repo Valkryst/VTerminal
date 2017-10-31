823cf53f3494de567718375469b1d59a3d5127d2
Valkryst <valkryst@valkryst.com>
2017-10-31 10:53:12 -0300

    Adds Tint/ShadeShader

---
+++ src/com/valkryst/VTerminal/shader/ShadeShader.java
@@ -0,0 +1,53 @@
+package com.valkryst.VTerminal.shader;
+
+import com.valkryst.VTerminal.AsciiCharacter;
+import com.valkryst.VTerminal.AsciiTile;
+import com.valkryst.VTerminal.misc.ColorFunctions;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+@Data
+public class ShadeShader implements Shader {
+    /** Whether or not to shade the background color. */
+    private boolean shadeBackground = false;
+    /** Whether or not to shade the foreground color. */
+    private boolean shadeForeground = false;
+    /** The factor to shade the background color by. */
+    private double backgroundShadeFactor = 0.0;
+    /** The factor to shade the foreground color by. */
+    private double foregroundShadeFactor = 0.0;
+
+    @Override
+    public BufferedImage run(@NonNull BufferedImage image, final @NonNull AsciiCharacter character) {
+        if (character instanceof AsciiTile) {
+            return image;
+        }
+
+        if (shadeBackground) {
+            final Color originalColor = character.getBackgroundColor();
+            final Color shadeColor = ColorFunctions.shade(originalColor, backgroundShadeFactor);
+            image = swapColor(image, originalColor, shadeColor);
+        }
+
+        if (shadeForeground) {
+            final Color originalColor = character.getForegroundColor();
+            final Color shadeColor = ColorFunctions.shade(originalColor, foregroundShadeFactor);
+            image = swapColor(image, originalColor, shadeColor);
+        }
+
+        return image;
+    }
+
+    @Override
+    public Shader copy() {
+        final ShadeShader shadeShader = new ShadeShader();
+        shadeShader.setShadeBackground(shadeBackground);
+        shadeShader.setShadeForeground(shadeForeground);
+        shadeShader.setBackgroundShadeFactor(backgroundShadeFactor);
+        shadeShader.setForegroundShadeFactor(foregroundShadeFactor);
+        return shadeShader;
+    }
+}
---
+++ src/com/valkryst/VTerminal/shader/TintShader.java
@@ -0,0 +1,53 @@
+package com.valkryst.VTerminal.shader;
+
+import com.valkryst.VTerminal.AsciiCharacter;
+import com.valkryst.VTerminal.AsciiTile;
+import com.valkryst.VTerminal.misc.ColorFunctions;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+@Data
+public class TintShader implements Shader {
+    /** Whether or not to tint the background color. */
+    private boolean tintBackground = false;
+    /** Whether or not to tint the foreground color. */
+    private boolean tintForeground = false;
+    /** The factor to tint the background color by. */
+    private double backgroundTintFactor = 0.0;
+    /** The factor to tint the foreground color by. */
+    private double foregroundTintFactor = 0.0;
+
+    @Override
+    public BufferedImage run(@NonNull BufferedImage image, final @NonNull AsciiCharacter character) {
+        if (character instanceof AsciiTile) {
+            return image;
+        }
+
+        if (tintBackground) {
+            final Color originalColor = character.getBackgroundColor();
+            final Color tintedColor = ColorFunctions.tint(originalColor, backgroundTintFactor);
+            image = swapColor(image, originalColor, tintedColor);
+        }
+
+        if (tintForeground) {
+            final Color originalColor = character.getForegroundColor();
+            final Color tintedColor = ColorFunctions.tint(originalColor, foregroundTintFactor);
+            image = swapColor(image, originalColor, tintedColor);
+        }
+
+        return image;
+    }
+
+    @Override
+    public Shader copy() {
+        final TintShader tintShader = new TintShader();
+        tintShader.setTintBackground(tintBackground);
+        tintShader.setTintForeground(tintForeground);
+        tintShader.setBackgroundTintFactor(backgroundTintFactor);
+        tintShader.setForegroundTintFactor(foregroundTintFactor);
+        return tintShader;
+    }
+}
