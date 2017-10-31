a611f3b8a7ec5463c4331e1edd2f9e379dd38e49
Valkryst <valkryst@valkryst.com>
2017-10-31 10:58:35 -0300

    Rewrites test.

--- test/com/valkryst/VTerminal/samples/SampleStringOperations.java
+++ test/com/valkryst/VTerminal/samples/SampleStringOperations.java
@@ -2,13 +2,14 @@ package com.valkryst.VTerminal.samples;
 
 import com.valkryst.VTerminal.Panel;
 import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VTerminal.builder.component.LabelBuilder;
+import com.valkryst.VTerminal.component.Label;
 import com.valkryst.VTerminal.component.Screen;
 import com.valkryst.VTerminal.font.Font;
 import com.valkryst.VTerminal.font.FontLoader;
 import com.valkryst.VTerminal.shader.FlipShader;
 
 import java.awt.Color;
-import java.awt.Point;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -25,45 +26,100 @@ public class SampleStringOperations {
 
         Thread.sleep(50);
 
-        screen.write("Writing a String on Line #3", new Point(0, 2));
+        final LabelBuilder labelBuilder = new LabelBuilder();
+        labelBuilder.setColumnIndex(0);
+        labelBuilder.setRowIndex(0);
 
-        screen.write("Horizontally Flipping a String on Line #4", new Point(0, 3));
-        screen.getString(3).addShaders(new FlipShader(true, false));
 
-        screen.write("Vertically Flipping a String on Line #5", new Point(0, 4));
-        screen.getString(4).addShaders(new FlipShader(false, true));
+        labelBuilder.setText("Writing a regular string.");
+        Label label = labelBuilder.build();
+        screen.addComponent(label);
 
-        screen.write("Vertically & Horizontally Flipping a String on Line #6", new Point(0, 5));
-        screen.getString(5).addShaders(new FlipShader(true, true));
 
-        screen.write("Applying a Tint Gradient on Line #7", new Point(0, 6));
-        screen.getString(6).applyTintGradient(Color.CYAN, true);
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a horizontally flipped string.");
+        label = labelBuilder.build();
+        label.addShaders(new FlipShader(true, false));
+        screen.addComponent(label);
 
-        screen.write("Applying a Shade Gradient on Line #8", new Point(0, 7));
-        screen.getString(7).applyShadeGradient(Color.CYAN, true);
 
-        screen.write("Applying a Background Color Gradient on Line #9", new Point(0, 8));
-        screen.getString(8).applyColorGradient(Color.CYAN, Color.PINK, true);
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a vertically flipped string.");
+        label = labelBuilder.build();
+        label.addShaders(new FlipShader(false, true));
+        screen.addComponent(label);
 
-        screen.write("Applying a Foreground Color Gradient on Line #10", new Point(0, 9));
-        screen.getString(9).applyColorGradient(Color.CYAN, Color.PINK, false);
 
-        screen.write("Applying a Back/Foreground Color Gradient on Line #11", new Point(0, 10));
-        screen.getString(10).applyColorGradient(Color.RED, Color.GREEN, false);
-        screen.getString(10).applyColorGradient(Color.CYAN, Color.PINK, true);
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a horizontally & vertically flipped string.");
+        label = labelBuilder.build();
+        label.addShaders(new FlipShader(true, true));
+        screen.addComponent(label);
 
-        screen.write("Changing Background Color on Line #12", new Point(0, 11));
-        screen.getString(11).setBackgroundColor(Color.RED);
 
-        screen.write("Changing Foreground Color on Line #13", new Point(0, 12));
-        screen.getString(12).setForegroundColor(Color.RED);
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a tint gradient.");
+        label = labelBuilder.build();
+        label.getString(0).applyTintGradient(true);
+        screen.addComponent(label);
 
-        screen.write("Changing Back/Foreground Color on Line #14", new Point(0, 13));
-        screen.getString(13).setBackgroundColor(Color.RED);
-        screen.getString(13).setForegroundColor(Color.BLUE);
 
-        screen.write("Applying Underline on Line #15", new Point(0, 14));
-        screen.getString(14).setUnderlined(true);
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a shade gradient.");
+        label = labelBuilder.build();
+        label.getString(0).applyShadeGradient(true);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a background color gradient.");
+        label = labelBuilder.build();
+        label.getString(0).applyColorGradient(Color.CYAN, Color.PINK, true);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a foreground color gradient.");
+        label = labelBuilder.build();
+        label.getString(0).applyColorGradient(Color.CYAN, Color.PINK, false);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a foreground color gradient.");
+        label = labelBuilder.build();
+        label.getString(0).applyColorGradient(Color.RED, Color.GREEN, false);
+        label.getString(0).applyColorGradient(Color.CYAN, Color.PINK, false);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a different background color.");
+        label = labelBuilder.build();
+        label.getString(0).setBackgroundColor(Color.RED);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a different foreground color.");
+        label = labelBuilder.build();
+        label.getString(0).setForegroundColor(Color.RED);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing a string with a different back/foreground color.");
+        label = labelBuilder.build();
+        label.getString(0).setForegroundColor(Color.RED);
+        label.getString(0).setForegroundColor(Color.BLUE);
+        screen.addComponent(label);
+
+
+        labelBuilder.setRowIndex(labelBuilder.getRowIndex() + 1);
+        labelBuilder.setText("Writing an underlined string.");
+        label = labelBuilder.build();
+        label.getString(0).setUnderlined(true);
+        screen.addComponent(label);
 
         panel.draw();
     }
