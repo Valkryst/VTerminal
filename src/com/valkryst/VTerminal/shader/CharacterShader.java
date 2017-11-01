3cfeca9da819a6861d70f9961076d0d81d51111a
Valkryst <valkryst@valkryst.com>
2017-10-31 21:49:17 -0300

    Adds CharacterShader

---
+++ src/com/valkryst/VTerminal/shader/CharacterShader.java
@@ -0,0 +1,30 @@
+package com.valkryst.VTerminal.shader;
+
+import com.valkryst.VTerminal.AsciiCharacter;
+import lombok.NonNull;
+
+import java.awt.image.BufferedImage;
+
+public interface CharacterShader extends Shader {
+    @Override
+    default BufferedImage run(final @NonNull BufferedImage image) {
+        throw new IllegalStateException("A CharacterShader must be run using the run(BufferedImage, AsciiCharacter) function.");
+    }
+
+    /**
+     * Runs the shader on a character image.
+     *
+     * @param image
+     *          The character image.
+     *
+     * @param character
+     *          The character.
+     *
+     * @return
+     *          The processed character image.
+     *
+     * @throws NullPointerException
+     *           If the image or character is null.
+     */
+    BufferedImage run(final @NonNull BufferedImage image, final @NonNull AsciiCharacter character);
+}
