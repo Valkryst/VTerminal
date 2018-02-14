9a453bd947cf98b449b23bd24b6f92c7ec670f22
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-02-14 09:14:53 -0400

    Adds tests for the getWidth and getHeight functions of the TileGrid class.

---
+++ test/com/valkryst/VTerminal/component/TileGridTest/GetHeightTest.java
@@ -0,0 +1,23 @@
+package com.valkryst.VTerminal.component.TileGridTest;
+
+import com.valkryst.VTerminal.AsciiTile;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GetHeightTest {
+    @Test
+    public void testGetHeight_ensureValueIsAsExpected() {
+        final int length = StaticGrid.TILE_GRID.getColumn(0).length;
+        Assert.assertEquals(6, length);
+    }
+
+    @Test
+    public void testGetHeight_ensureAllColumnsAreOfEqualLength() {
+        final int length = StaticGrid.TILE_GRID.getColumn(0).length;
+
+        for (int x = 0 ; x < StaticGrid.TILE_GRID.getHeight() ; x++) {
+            final AsciiTile[] column = StaticGrid.TILE_GRID.getColumn(x);
+            Assert.assertEquals(column.length, length);
+        }
+    }
+}
