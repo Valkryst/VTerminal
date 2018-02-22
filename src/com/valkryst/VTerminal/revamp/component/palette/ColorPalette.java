d16691ad5a30788c7cd565591d24e91c6c7bf1d1
Valkryst <valkryst@valkryst.com>
2018-02-22 10:46:11 -0400

    Fixes issue with CheckBox colors.

--- src/com/valkryst/VTerminal/revamp/component/palette/ColorPalette.java
+++ src/com/valkryst/VTerminal/revamp/component/palette/ColorPalette.java
@@ -9,17 +9,17 @@ public class ColorPalette {
     @Getter private Color defaultBackground =  new Color(45, 45, 45, 255);
     @Getter private Color defaultForeground = new Color(0xFFF9CA00, true);
 
-    @Getter private Color button_defaultBackground = new Color(45, 45, 45, 255);
+    @Getter private Color button_defaultBackground = defaultBackground;
     @Getter private Color button_defaultForeground = new Color(0xFF2DBEFF, true);
     @Getter private Color button_hoverBackground = new Color(0xFF2DFF63, true);
     @Getter private Color button_hoverForeground = ColorFunctions.shade(button_hoverBackground, 0.5);
     @Getter private Color button_pressedBackground = ColorFunctions.shade(button_hoverBackground, 0.25);
     @Getter private Color button_pressedForeground = ColorFunctions.shade(button_hoverForeground, 0.25);
 
-    @Getter private Color checkBox_defaultBackground = new Color(45, 45, 45, 255);
-    @Getter private Color checkBox_defaultForeground = new Color(0xFF2DBEFF, true);
-    @Getter private Color checkBox_hoverBackground = new Color(0xFF2DFF63, true);
-    @Getter private Color checkBox_hoverForeground = ColorFunctions.shade(button_hoverBackground, 0.5);
-    @Getter private Color checkBox_pressedBackground = ColorFunctions.shade(button_hoverBackground, 0.25);
+    @Getter private Color checkBox_defaultBackground = button_defaultBackground;
+    @Getter private Color checkBox_defaultForeground = defaultForeground;
+    @Getter private Color checkBox_hoverBackground = button_hoverBackground;
+    @Getter private Color checkBox_hoverForeground = button_hoverForeground;
+    @Getter private Color checkBox_pressedBackground = button_defaultBackground;
     @Getter private Color checkBox_pressedForeground = defaultForeground;
 }
