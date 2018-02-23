55c31d11b91602b6038a88f3e278b26c660d7341
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-02-23 10:43:57 -0400

    Rewrites LinePrinter to print onto a TileGrid, rather than a Screen or Component.

--- src/com/valkryst/VTerminal/printer/LinePrinter.java
+++ src/com/valkryst/VTerminal/printer/LinePrinter.java
@@ -1,7 +1,7 @@
 package com.valkryst.VTerminal.printer;
 
 import com.valkryst.VTerminal.Tile;
-import com.valkryst.VTerminal.component.Component;
+import com.valkryst.VTerminal.TileGrid;
 import com.valkryst.VTerminal.misc.ShapeAlgorithms;
 import lombok.*;
 
@@ -14,40 +14,41 @@ public class LinePrinter {
     @Getter @Setter private char printChar = '█';
 
     /**
-     * Prints one or more lines on a component using a set of points.
-     *
-     * The algorithm requires a minimum of two points.
+     * Prints one, or more, lines on a tile grid using a set of points.
      *
      * The algorithm draws a line from the first point to the second point,
      * then from the second point to the third point, and so on.
      *
-     * @param component
-     *        The component.
+     * @param grid
+     *        The grid.
      *
      * @param points
      *        The points.
      *
+     * @throws IllegalArgumentException
+     *         If there are fewer than two points.
+     *
      * @throws NullPointerException
      *         If the screen or points array is null.
      */
-    public void print(final @NonNull Component component, final @NonNull Point[] points) {
+    public void print(final @NonNull TileGrid grid, final @NonNull Point[] points) {
         if (points.length < 2) {
-            return;
+            throw new IllegalArgumentException("A line requires two points to be drawn.");
         }
 
         for (int i = 1 ; i < points.length ; i++) {
             final Point previous = points[i - 1];
             final Point current = points[i];
 
-            print(component, previous, current);
+            print(grid, previous, current);
         }
     }
 
     /**
-     * Prints a line on a component.
+     * Prints a line on a tile grid.
      *
-     * @param component
-     *        The component.
+     * @param grid
+     *        The grid.
      *
      * @param from
      *         The start point of the line.
@@ -58,9 +59,9 @@ public class LinePrinter {
      * @throws NullPointerException
      *         If the component or either point is null.
      */
-    public void print(final @NonNull Component component, final Point from, final Point to) {
+    public void print(final @NonNull TileGrid grid, final Point from, final Point to) {
         for (final Point point : ShapeAlgorithms.getLine(from.x, from.y, to.x, to.y)) {
-            final Tile tile = component.getTiles().getTileAt(point);
+            final Tile tile = grid.getTileAt(point);
 
             if (tile != null) {
                 tile.setCharacter(printChar);
