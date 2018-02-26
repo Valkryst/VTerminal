fed705f4dbbe34915729701f9715a433aafc1c85
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-02-26 13:58:55 -0400

    Removes unnecessary exception throwing.

--- test/com/valkryst/VTerminal/font/FontLoaderTest.java
+++ test/com/valkryst/VTerminal/font/FontLoaderTest.java
@@ -5,45 +5,44 @@ import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 
 public class FontLoaderTest {
     private final String spriteSheetPath = "Fonts/DejaVu Sans Mono/20pt/bitmap.png";
     private final String characterDataPath = "Fonts/DejaVu Sans Mono/20pt/data.fnt";
 
     @Test(expected=FileNotFoundException.class)
-    public void loadFont_strings() throws IOException, URISyntaxException {
+    public void loadFont_strings() throws IOException {
         FontLoader.loadFont(spriteSheetPath, characterDataPath, 1);
     }
 
 
     @Test(expected=FileNotFoundException.class)
-    public void loadFont_strings_withScaleBelowOne() throws IOException, URISyntaxException {
+    public void loadFont_strings_withScaleBelowOne() throws IOException {
         FontLoader.loadFont(spriteSheetPath, characterDataPath, 0);
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFont_strings_withNullSpriteSheetPath() throws IOException, URISyntaxException {
+    public void loadFont_strings_withNullSpriteSheetPath() throws IOException {
         FontLoader.loadFont(null, characterDataPath, 1);
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFont_strings_withNullCharacterDataPath() throws IOException, URISyntaxException {
+    public void loadFont_strings_withNullCharacterDataPath() throws IOException {
         FontLoader.loadFont(spriteSheetPath, null, 1);
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void loadFont_strings_withEmptySpriteSheetPath() throws IOException, URISyntaxException {
+    public void loadFont_strings_withEmptySpriteSheetPath() throws IOException {
         FontLoader.loadFont("", characterDataPath, 1);
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void loadFont_strings_withEmptyCharacterDataPath() throws IOException, URISyntaxException {
+    public void loadFont_strings_withEmptyCharacterDataPath() throws IOException {
         FontLoader.loadFont(spriteSheetPath, "", 1);
     }
 
     @Test
-    public void loadFont_streams() throws IOException, URISyntaxException {
+    public void loadFont_streams() throws IOException {
         final ClassLoader classLoader = FontLoader.class.getClassLoader();
 
         final InputStream spriteSheetStream = classLoader.getResourceAsStream(spriteSheetPath);
@@ -53,7 +52,7 @@ public class FontLoaderTest {
     }
 
     @Test
-    public void loadFont_streams_withScaleBelowOne() throws IOException, URISyntaxException {
+    public void loadFont_streams_withScaleBelowOne() throws IOException {
         final ClassLoader classLoader = FontLoader.class.getClassLoader();
 
         final InputStream spriteSheetStream = classLoader.getResourceAsStream(spriteSheetPath);
@@ -63,7 +62,7 @@ public class FontLoaderTest {
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFont_streams_withNullSpriteSheetInputStream() throws IOException, URISyntaxException {
+    public void loadFont_streams_withNullSpriteSheetInputStream() throws IOException {
         final ClassLoader classLoader = FontLoader.class.getClassLoader();
 
         final InputStream characterDataStream = classLoader.getResourceAsStream(characterDataPath);
@@ -72,7 +71,7 @@ public class FontLoaderTest {
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFont_streams_withNullCharacterDataInputStream() throws IOException, URISyntaxException {
+    public void loadFont_streams_withNullCharacterDataInputStream() throws IOException {
         final ClassLoader classLoader = FontLoader.class.getClassLoader();
 
         final InputStream spriteSheetStream = classLoader.getResourceAsStream(spriteSheetPath);
@@ -81,27 +80,27 @@ public class FontLoaderTest {
     }
 
     @Test
-    public void loadFontFromJar() throws IOException, URISyntaxException {
+    public void loadFontFromJar() throws IOException {
         FontLoader.loadFontFromJar(spriteSheetPath, characterDataPath, 1);
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFontFromJar_withNullSpriteSheetPath() throws IOException, URISyntaxException {
+    public void loadFontFromJar_withNullSpriteSheetPath() throws IOException {
         FontLoader.loadFontFromJar(null, characterDataPath, 1);
     }
 
     @Test(expected=NullPointerException.class)
-    public void loadFontFromJar_withNullCharacterDataPath() throws IOException, URISyntaxException {
+    public void loadFontFromJar_withNullCharacterDataPath() throws IOException {
         FontLoader.loadFontFromJar(spriteSheetPath, null, 1);
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void loadFontFromJar_withEmptySpriteSheetPath() throws IOException, URISyntaxException {
+    public void loadFontFromJar_withEmptySpriteSheetPath() throws IOException {
         FontLoader.loadFontFromJar("", characterDataPath, 1);
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void loadFontFromJar_withEmptyCharacterDataPath() throws IOException, URISyntaxException {
+    public void loadFontFromJar_withEmptyCharacterDataPath() throws IOException {
         FontLoader.loadFontFromJar(spriteSheetPath, "", 1);
     }
 }
