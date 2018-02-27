1807f69bbdd59d893463df089b9fcc7104910eb3
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-02-27 10:07:06 -0400

    Sets all color instance variables to final.

--- src/com/valkryst/VTerminal/palette/P21PhosphorColorPalette.java
+++ src/com/valkryst/VTerminal/palette/P21PhosphorColorPalette.java
@@ -6,40 +6,40 @@ import lombok.Getter;
 import java.awt.Color;
 
 public class P21PhosphorColorPalette extends ColorPalette {
-    @Getter private Color defaultBackground = Color.BLACK;
-    @Getter private Color defaultForeground = new Color(255, 156, 0, 255);
+    @Getter private final Color defaultBackground = Color.BLACK;
+    @Getter private final Color defaultForeground = new Color(255, 156, 0, 255);
 
-    @Getter private Color button_defaultBackground = defaultBackground;
-    @Getter private Color button_defaultForeground = defaultForeground;
-    @Getter private Color button_hoverBackground = defaultForeground;
-    @Getter private Color button_hoverForeground = ColorFunctions.shade(button_hoverBackground, 0.5);
-    @Getter private Color button_pressedBackground = ColorFunctions.shade(button_hoverBackground, 0.25);
-    @Getter private Color button_pressedForeground = ColorFunctions.shade(button_hoverForeground, 0.25);
+    @Getter private final Color button_defaultBackground = defaultBackground;
+    @Getter private final Color button_defaultForeground = defaultForeground;
+    @Getter private final Color button_hoverBackground = defaultForeground;
+    @Getter private final Color button_hoverForeground = ColorFunctions.shade(button_hoverBackground, 0.5);
+    @Getter private final Color button_pressedBackground = ColorFunctions.shade(button_hoverBackground, 0.25);
+    @Getter private final Color button_pressedForeground = ColorFunctions.shade(button_hoverForeground, 0.25);
 
-    @Getter private Color checkBox_defaultBackground = button_defaultBackground;
-    @Getter private Color checkBox_defaultForeground = button_defaultForeground;
-    @Getter private Color checkBox_hoverBackground = button_hoverBackground;
-    @Getter private Color checkBox_hoverForeground = button_hoverForeground;
-    @Getter private Color checkBox_checkedBackground = button_defaultBackground;
-    @Getter private Color checkBox_checkedForeground = defaultForeground;
+    @Getter private final Color checkBox_defaultBackground = button_defaultBackground;
+    @Getter private final Color checkBox_defaultForeground = button_defaultForeground;
+    @Getter private final Color checkBox_hoverBackground = button_hoverBackground;
+    @Getter private final Color checkBox_hoverForeground = button_hoverForeground;
+    @Getter private final Color checkBox_checkedBackground = button_defaultBackground;
+    @Getter private final Color checkBox_checkedForeground = defaultForeground;
 
-    @Getter private Color layer_defaultBackground = defaultBackground;
-    @Getter private Color layer_defaultForeground = defaultForeground;
+    @Getter private final Color layer_defaultBackground = defaultBackground;
+    @Getter private final Color layer_defaultForeground = defaultForeground;
 
-    @Getter private Color progressBar_incompleteBackground = defaultBackground;
-    @Getter private Color progressBar_incompleteForeground = ColorFunctions.shade(defaultForeground, 0.5);
-    @Getter private Color progressBar_completeBackground = defaultBackground;
-    @Getter private Color progressBar_completeForeground = defaultForeground;
+    @Getter private final Color progressBar_incompleteBackground = defaultBackground;
+    @Getter private final Color progressBar_incompleteForeground = ColorFunctions.shade(defaultForeground, 0.5);
+    @Getter private final Color progressBar_completeBackground = defaultBackground;
+    @Getter private final Color progressBar_completeForeground = defaultForeground;
 
-    @Getter private Color radioButton_defaultBackground = button_defaultBackground;
-    @Getter private Color radioButton_defaultForeground = button_defaultForeground;
-    @Getter private Color radioButton_hoverBackground = button_hoverBackground;
-    @Getter private Color radioButton_hoverForeground = button_hoverForeground;
-    @Getter private Color radioButton_pressedBackground = defaultBackground;
-    @Getter private Color radioButton_pressedForeground = defaultForeground;
+    @Getter private final Color radioButton_defaultBackground = button_defaultBackground;
+    @Getter private final Color radioButton_defaultForeground = button_defaultForeground;
+    @Getter private final Color radioButton_hoverBackground = button_hoverBackground;
+    @Getter private final Color radioButton_hoverForeground = button_hoverForeground;
+    @Getter private final Color radioButton_pressedBackground = defaultBackground;
+    @Getter private final Color radioButton_pressedForeground = defaultForeground;
 
-    @Getter private Color textArea_defaultBackground = new Color(0xFF68D0FF, true);
-    @Getter private Color textArea_defaultForeground = new Color(0xFF8E999E, true);
-    @Getter private Color textArea_caretBackground = textArea_defaultForeground;
-    @Getter private Color textArea_caretForeground = textArea_defaultBackground;
+    @Getter private final Color textArea_defaultBackground = new Color(0xFF68D0FF, true);
+    @Getter private final Color textArea_defaultForeground = new Color(0xFF8E999E, true);
+    @Getter private final Color textArea_caretBackground = textArea_defaultForeground;
+    @Getter private final Color textArea_caretForeground = textArea_defaultBackground;
 }
