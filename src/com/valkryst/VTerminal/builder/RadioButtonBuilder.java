838078c5b44a42eb915c1209a4cc42a7baa9fcd6
Valkryst <valkryst@valkryst.com>
2018-09-11 18:48:00 -0300

    Removes unnecessary annotations. Removes duplicated text instance variable. The super class contains one. Rewrites the checkState function so that it creates a new RadioButtonGroup if one isn't set.

--- src/com/valkryst/VTerminal/builder/RadioButtonBuilder.java
+++ src/com/valkryst/VTerminal/builder/RadioButtonBuilder.java
@@ -4,18 +4,13 @@ import com.valkryst.VJSON.VJSONParser;
 import com.valkryst.VTerminal.component.RadioButton;
 import com.valkryst.VTerminal.component.RadioButtonGroup;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.json.simple.JSONObject;
 
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper=true)
 public class RadioButtonBuilder extends ButtonBuilder implements VJSONParser {
-    /** The text of the label to display to the right of the radio button. */
-    @NonNull private String text;
-
     /** The character to display when the radio button is not checked. */
     private char emptyButtonChar;
     /** The character to display when the radio button is checked. */
@@ -28,9 +23,9 @@ public class RadioButtonBuilder extends ButtonBuilder implements VJSONParser {
     public RadioButton build() {
         checkState();
 
-        super.setDimensions(text.length() + 2, 1);
+        super.setDimensions(super.getText().length() + 2, 1);
 
-        text = emptyButtonChar + " " + text;
+        super.setText(emptyButtonChar + " " + super.getText());
 
         final RadioButton radioButton = new RadioButton(this);
         group.addRadioButton(radioButton);
@@ -38,17 +33,12 @@ public class RadioButtonBuilder extends ButtonBuilder implements VJSONParser {
         return radioButton;
     }
 
-    /**
-     * Checks the current state of the builder.
-     *
-     * @throws NullPointerException
-     *          If the radio or group are null.
-     */
+    /** Checks the current state of the builder. */
     protected void checkState() throws NullPointerException {
         super.checkState();
 
         if (group == null) {
-            throw new NullPointerException("The button must belong to a radio button group.");
+            group = new RadioButtonGroup();
         }
     }
 
@@ -60,16 +50,9 @@ public class RadioButtonBuilder extends ButtonBuilder implements VJSONParser {
 
         super.parse(jsonObject);
 
-        final String text = getString(jsonObject, "text");
         final Character emptyButtonChar = getChar(jsonObject, "emptyButtonChar");
         final Character checkedButtonChar = getChar(jsonObject, "checkedButtonChar");
         // todo Add isChecked
-
-        if (text == null) {
-            throw new NullPointerException("The 'text' value was not found.");
-        } else {
-            this.text = text;
-        }
 
         if (emptyButtonChar == null) {
             throw new NullPointerException("The 'emptyButtonChar' value was not found.");
@@ -87,8 +70,6 @@ public class RadioButtonBuilder extends ButtonBuilder implements VJSONParser {
     @Override
     public void reset() {
         super.reset();
-
-        text = "";
 
         emptyButtonChar = '○';
         checkedButtonChar = '◉';
