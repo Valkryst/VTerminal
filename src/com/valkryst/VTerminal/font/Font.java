036d28a375a4f5f6ac549c4b1d317d53386d6a3d
Valkryst <valkryst@valkryst.com>
2018-11-12 16:40:37 -0400

    Adds checks and support for full-width characters.

--- src/com/valkryst/VTerminal/font/Font.java
+++ src/com/valkryst/VTerminal/font/Font.java
@@ -1,74 +1,148 @@
 package com.valkryst.VTerminal.font;
 
-import lombok.Getter;
 import lombok.NonNull;
 import lombok.ToString;
+import org.apache.logging.log4j.LogManager;
 
-import java.awt.Dimension;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ToString
 public class Font {
-    /**
-     * The sprite-images of every character provided by the Font with a completely
-     * transparent background.
-     */
-    private final HashMap<Character, BufferedImage> characterImages;
+    /** The characters provided by the font. */
+    private final HashMap<Character, FontCharacter> fontCharacters;
 
-    /** The width/height of the font. */
-    @Getter private final Dimension dimensions;
+    /** The dimensions of the most-common character size supplied by the font. */
+    private final Dimension baseDimensions;
 
     /**
      * Constructs a new Font.
      *
-     * @param characterImages
-     *        The base character images.
+     * @param fontCharacters
+     *          The font characters.
      *
      * @param scale
-     *        The amount to scale each character image by.
+     *          The amount to scale each character image by.
      *
      * @throws NullPointerException
-     *         If the characterImages is null.
+     *           If the characterImages is null.
      */
-    public Font(final @NonNull HashMap<Character, BufferedImage> characterImages, final double scale) {
-        this.characterImages = characterImages;
+    public Font(final @NonNull HashMap<Character, FontCharacter> fontCharacters, final double scale) {
+        this.fontCharacters = fontCharacters;
 
-        final int width = (int) (characterImages.get('X').getWidth() * scale);
-        final int height = (int) (characterImages.get('X').getHeight() * scale);
-        dimensions = new Dimension(width, height);
+        final HashMap<Integer, Integer> widthCounters = new HashMap<>();
+        final HashMap<Integer, Integer> heightCounters = new HashMap<>();
 
-        resize(scale, scale);
+        for (final Map.Entry<Character, FontCharacter> entry : fontCharacters.entrySet()) {
+            final FontCharacter fontCharacter = entry.getValue();
+
+            // Resizes the character images.
+            fontCharacter.resizeImage(scale, scale);
+
+            // Count occurrences of width/height dimensions.
+            final int width = fontCharacter.getWidth();
+            final int height = fontCharacter.getHeight();
+
+            if (widthCounters.containsKey(width)) {
+                final int count = widthCounters.get(width);
+                widthCounters.put(width, count + 1);
+            } else {
+                widthCounters.put(width, 0);
+            }
+
+            if (heightCounters.containsKey(height)) {
+                final int count = heightCounters.get(height);
+                heightCounters.put(height, count + 1);
+            } else {
+                heightCounters.put(height, 0);
+            }
+        }
+
+        // Calculate most common width/height dimensions.
+        Map.Entry<Integer, Integer> width = null;
+        Map.Entry<Integer, Integer> height = null;
+
+        for (final Map.Entry<Integer, Integer> entry : widthCounters.entrySet()) {
+            if (width == null) {
+                width = entry;
+            }
+
+            if (width.getValue() < entry.getValue()) {
+                width = entry;
+            }
+        }
+
+        for (final Map.Entry<Integer, Integer> entry : heightCounters.entrySet()) {
+            if (height == null) {
+                height = entry;
+            }
+
+            if (height.getValue() < entry.getValue()) {
+                height = entry;
+            }
+        }
+
+        // Create base dimensions.
+        final int baseWidth = (width == null ? 1 : width.getKey());
+        final int baseHeight = (height == null ? 1 : height.getKey());
+        baseDimensions = new Dimension(baseWidth, baseHeight);
+
+        /*
+         * Search for any characters which have dimensions that are not divisible by the most common width
+         * and height.
+         */
+        final List<FontCharacter> invalidCharacters = new ArrayList<>();
+
+        for (final Map.Entry<Character, FontCharacter> entry : fontCharacters.entrySet()) {
+            final FontCharacter fontCharacter = entry.getValue();
+            final int fcWidth = fontCharacter.getWidth();
+            final int fcHeight = fontCharacter.getHeight();
+            boolean isValid = true;
+
+
+            if (fcWidth < baseWidth) {
+                LogManager.getLogger().error("Font Error:");
+                LogManager.getLogger().error("\n\t" + fontCharacter);
+                LogManager.getLogger().error("\n\tThe character's width is less than the font's base width of " + baseWidth + ".");
+                isValid = false;
+            }
+
+            if (fcHeight < baseHeight) {
+                LogManager.getLogger().error("Font Error:");
+                LogManager.getLogger().error("\n\t" + fontCharacter);
+                LogManager.getLogger().error("\n\tThe character's height is less than the font's base height of " + baseHeight + ".");
+                isValid = false;
+            }
+
+            if (fcWidth % baseWidth != 0) {
+                LogManager.getLogger().error("Font Error:");
+                LogManager.getLogger().error("\n\t" + fontCharacter);
+                LogManager.getLogger().error("\n\tThe character's width is not divisible by the font's base width " + baseWidth + ".");
+                isValid = false;
+            }
+
+            if (fcHeight % baseHeight != 0) {
+                LogManager.getLogger().error("Font Error:");
+                LogManager.getLogger().error("\n\t" + fontCharacter);
+                LogManager.getLogger().error("\n\tThe character's height is not divisible by the font's base height " + baseHeight + ".");
+                isValid = false;
+            }
+
+            if (isValid == false) {
+                invalidCharacters.add(fontCharacter);
+            }
+        }
+
+        invalidCharacters.forEach(fontCharacters::remove);
     }
 
-    /**
-     * Resizes the font.
-     *
-     * You probably shouldn't be calling this function, it exists specifically
-     * for when full screen mode is enabled and the font needs to be
-     * resized to fit the screen.
-     *
-     * @param scaleX
-     *          The amount to scale the font width by.
-     *
-     * @param scaleY
-     *          The amount to scale the font height by.
-     */
-    public void resize(final double scaleX, final double scaleY) {
-        if (scaleX > 0 && scaleY > 0) {
-            final int width = (int) (characterImages.get('X').getWidth() * scaleX);
-            final int height = (int) (characterImages.get('X').getHeight() * scaleY);
-            dimensions.setSize(width, height);
-
-            final AffineTransform tx = AffineTransform.getScaleInstance(scaleX, scaleY);
-            final AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-
-            for (final Map.Entry<Character, BufferedImage> entry : characterImages.entrySet()) {
-                characterImages.put(entry.getKey(), op.filter(entry.getValue(), null));
-            }
+    public void resize(final double scaleWidth, final double scaleHeight) {
+        for (final Map.Entry<Character, FontCharacter> entry : fontCharacters.entrySet()) {
+            entry.getValue().resizeImage(scaleWidth, scaleHeight);
         }
     }
 
@@ -85,7 +159,7 @@ public class Font {
      *         Whether or not the character is supported.
      */
     public boolean isCharacterSupported(final char character) {
-        return characterImages.containsKey(character);
+        return fontCharacters.containsKey(character);
     }
 
     /**
@@ -98,26 +172,26 @@ public class Font {
      *         The image.
      */
     public BufferedImage getCharacterImage(final char character) {
-        return characterImages.get(character);
+        return fontCharacters.get(character).getImage();
     }
 
     /**
-     * Retrieves the width of a character cell.
+     * Retrieves the most common width of a character cell.
      *
      * @return
-     *         The width of a character cell.
+     *         The most common width of a character cell.
      */
     public int getWidth() {
-        return dimensions.width;
+        return baseDimensions.width;
     }
 
     /**
-     * Retrieves the height of a character cell.
+     * Retrieves the most common height of a character cell.
      *
      * @return
-     *         The height of a character cell.
+     *         The most common height of a character cell.
      */
     public int getHeight() {
-        return dimensions.height;
+        return baseDimensions.height;
     }
 }
