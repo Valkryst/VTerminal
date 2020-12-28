b180a12b9fd4870eda943786eba31272f96b5303
Valkryst <valkryst@valkryst.com>
2020-12-28 11:30:50 -0400

    Update tests to JUnit5

--- src/test/java/com/valkryst/VTerminal/misc/ColorFunctionsTest.java
+++ src/test/java/com/valkryst/VTerminal/misc/ColorFunctionsTest.java
@@ -1,136 +1,146 @@
 package com.valkryst.VTerminal.misc;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import java.awt.Color;
-import java.util.concurrent.ThreadLocalRandom;
+import java.awt.*;
 
 public class ColorFunctionsTest {
-    @Test(expected=NullPointerException.class)
-    public void testShade_withNullColor() {
-        ColorFunctions.shade(null, 1);
-    }
-
     @Test
-    public void testShade_withAboveOneShadeFactor() {
-        final Color color = new Color(255, 255, 255);
-        final Color shadedColor = ColorFunctions.shade(color, 2);
-        Assert.assertEquals(Color.BLACK, shadedColor);
-    }
+    public void canShadeColor() {
+    	final var original = new Color[] {
+			new Color(255, 255, 255, 255),
+			new Color(255, 255, 255, 255),
+			new Color(255, 255, 255, 255),
+			new Color(255, 255, 255, 255),
+			new Color(255, 255, 255, 255)
+		};
+		final var shadeFactor = new double[] {
+			1.0,
+			0.75,
+			0.5,
+			0.25,
+			0.0
+		};
+    	final var expected = new Color[] {
+			new Color(0, 0, 0, 255),
+			new Color(63, 63, 63, 255),
+			new Color(127, 127, 127, 255),
+			new Color(191, 191, 191, 255),
+			new Color(255, 255, 255, 255)
+		};
 
-    @Test
-    public void testShade_withBelowZeroShadeFactor() {
-        final Color color = new Color(255, 255, 255);
-        final Color shadedColor = ColorFunctions.shade(color, -1);
-        Assert.assertEquals(color, shadedColor);
-    }
+		for (int i = 0 ; i < original.length ; i++) {
+			final var result = ColorFunctions.shade(original[i], shadeFactor[i]);
+			Assertions.assertEquals(expected[i], result);
+		}
+	}
 
-    @Test
-    public void testShade_withZeroShadeFactor() {
-        final Color color = new Color(255, 255, 255);
-        final Color shadedColor = ColorFunctions.shade(color, 0);
-        Assert.assertEquals(color, shadedColor);
-    }
+	@Test
+	public void canShadeColorWhenFactorIsAboveOne() {
+		final Color color = new Color(255, 255, 255);
+		final Color shadedColor = ColorFunctions.shade(color, 2);
+		Assertions.assertEquals(Color.BLACK, shadedColor);
+	}
 
-    @Test
-    public void testShade_withNonZeroAndNonOneShadeFactor() {
-        float randVal = 0;
+	@Test
+	public void canShadeColorWhenFactorIsBelowZero() {
+		final Color color = new Color(255, 255, 255);
+		final Color shadedColor = ColorFunctions.shade(color, 0);
+		Assertions.assertEquals(color, shadedColor);
+	}
 
-        while (randVal == 0 || randVal == 1) {
-            randVal = ThreadLocalRandom.current().nextFloat();
-        }
+	@Test
+	public void cannotShadeNullColor() {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			ColorFunctions.shade(null, 0.0);
+		});
+	}
 
-        final Color color = new Color(255, 255, 255);
-        final Color shadedColor = ColorFunctions.shade(color, randVal);
-        Assert.assertThat(color, CoreMatchers.is(CoreMatchers.not(shadedColor)));
-    }
+	@Test
+	public void canTintColor() {
+		final var original = new Color[] {
+			new Color(0, 0, 0, 255),
+			new Color(0, 0, 0, 255),
+			new Color(0, 0, 0, 255),
+			new Color(0, 0, 0, 255),
+			new Color(0, 0, 0, 255)
+		};
+		final var shadeFactor = new double[] {
+			1.0,
+			0.75,
+			0.5,
+			0.25,
+			0.0
+		};
+		final var expected = new Color[] {
+			new Color(255, 255, 255, 255),
+			new Color(191, 191, 191, 255),
+			new Color(127, 127, 127, 255),
+			new Color(63, 63, 63, 255),
+			new Color(0, 0, 0, 255)
+		};
 
-    @Test
-    public void testShade_withOneShadeFactor() {
-        final Color color = new Color(255, 255, 255);
-        final Color shadedColor = ColorFunctions.shade(color, 1);
-        Assert.assertEquals(Color.BLACK, shadedColor);
-    }
+		for (int i = 0 ; i < original.length ; i++) {
+			final var result = ColorFunctions.tint(original[i], shadeFactor[i]);
+			Assertions.assertEquals(expected[i], result);
+		}
+	}
 
-    @Test(expected=NullPointerException.class)
-    public void testTint_withNullColor() {
-        ColorFunctions.shade(null, 1);
-    }
+	@Test
+	public void canTintColorWhenFactorIsAboveOne() {
+		final Color color = new Color(255, 255, 255);
+		final Color shadedColor = ColorFunctions.tint(color, 2);
+		Assertions.assertEquals(Color.WHITE, shadedColor);
+	}
 
-    @Test
-    public void testTint_withAboveOneTintFactor() {
-        final Color color = new Color(255, 255, 255);
-        final Color tintedColor = ColorFunctions.tint(color, 2);
-        Assert.assertEquals(Color.WHITE, tintedColor);
-    }
+	@Test
+	public void canTintColorWhenFactorIsBelowZero() {
+		final Color color = new Color(255, 255, 255);
+		final Color shadedColor = ColorFunctions.tint(color, 0);
+		Assertions.assertEquals(color, shadedColor);
+	}
 
-    @Test
-    public void testTint_withBelowZeroTintFactor() {
-        final Color color = new Color(0, 0, 0);
-        final Color tintedColor = ColorFunctions.tint(color, -1);
-        Assert.assertEquals(Color.BLACK, tintedColor);
-    }
+	@Test
+	public void cannotTintNullColor() {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			ColorFunctions.tint(null, 0.0);
+		});
+	}
 
-    @Test
-    public void testTint_withZeroTintFactor() {
-        final Color color = new Color(0, 0, 0);
-        final Color tintedColor = ColorFunctions.tint(color, 0);
-        Assert.assertEquals(color, tintedColor);
-    }
+	@Test
+	public void canBlendAlphaWithColorObjects() {
+		final Color source = new Color(0, 255, 0, 100);
+		final Color destination = new Color(255, 0, 0, 255);
 
-    @Test
-    public void testTint_withNonZeroAndNonOneTintFactor() {
-        float randVal = 0;
+		final Color result = ColorFunctions.alphaBlend(source, destination);
+		final Color expected = new Color(101, 156, 0, 255);
 
-        while (randVal == 0 || randVal == 1) {
-            randVal = ThreadLocalRandom.current().nextFloat();
-        }
+		Assertions.assertEquals(expected, result);
+	}
 
-        final Color color = new Color(0, 0, 0);
-        final Color tintedColor = ColorFunctions.tint(color, randVal);
-        Assert.assertThat(color, CoreMatchers.is(CoreMatchers.not(tintedColor)));
-    }
+	@Test
+	public void canBlendAlphaWithInts() {
+		final Color source = new Color(0, 255, 0, 100);
+		final Color destination = new Color(255, 0, 0, 255);
 
-    @Test
-    public void testTint_withOneTintFactor() {
-        final Color color = new Color(0, 0, 0, 255);
-        final Color tintedColor = ColorFunctions.tint(color, 1);
-        Assert.assertEquals(Color.WHITE, tintedColor);
-    }
+		final int result = ColorFunctions.alphaBlend(source.getRGB(), destination.getRGB());
+		final Color expected = new Color(101, 156, 0, 255);
 
-    @Test(expected=NullPointerException.class)
-    public void testAlphaBlend_colorParams_withNullSource() {
-        final Color destination = new Color(0, 0, 0);
-        ColorFunctions.alphaBlend(null, destination);
-    }
+		Assertions.assertEquals(expected, new Color(result));
+	}
 
-    @Test(expected=NullPointerException.class)
-    public void testAlphaBlend_colorParams_withNullDestination() {
-        final Color source = new Color(255, 255, 255, 255);
-        ColorFunctions.alphaBlend(source, null);
-    }
+	@Test
+	public void cannotBlendAlphaWithNullSource() {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			ColorFunctions.alphaBlend(null, Color.BLACK);
+		});
+	}
 
-    @Test
-    public void testAlphaBlend_colorParams_withValidColors() {
-        final Color source = new Color(0, 255, 0, 100);
-        final Color destination = new Color(255, 0, 0, 255);
-
-        final Color result = ColorFunctions.alphaBlend(source, destination);
-        final Color expected = new Color(101, 156, 0, 255);
-
-        Assert.assertEquals(expected, result);
-    }
-
-    @Test
-    public void testAlphaBlend_intParams_withValidColors() {
-        final Color source = new Color(0, 255, 0, 100);
-        final Color destination = new Color(255, 0, 0, 255);
-
-        final int result = ColorFunctions.alphaBlend(source.getRGB(), destination.getRGB());
-        final Color expected = new Color(101, 156, 0, 255);
-
-        Assert.assertEquals(expected, new Color(result));
-    }
+	@Test
+	public void cannotBlendAlphaWithNullDestination() {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			ColorFunctions.alphaBlend(Color.BLACK, null);
+		});
+	}
 }
