ab6bf73c2ab887c2176c4f2c0230e1fd2edee935
Valkryst <github@valkryst.com>
2024-11-13 15:48:21 -0400

    Rewrite JavaDoc of `VColor`

--- src/main/java/com/valkryst/VTerminal/palette/VColor.java
+++ src/main/java/com/valkryst/VTerminal/palette/VColor.java
@@ -5,118 +5,65 @@ import lombok.NonNull;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 
+/** An extension of the {@link Color} class, which provides additional functionality. */
 public class VColor extends Color {
 	/**
-	 * Constructs a new instance of {@code VColor}, copying the RGBA components
-	 * of a given color.
+	 * Constructs a new {@link VColor}, copying the RGBA components of a given {@link Color}.
 	 *
-	 * @param color A color.
+	 * @param color {@link Color} to copy the RGBA components from.
 	 */
 	public VColor(final @NonNull Color color) {
 		super(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param red The red component.
-	 * @param green The green component
-	 * @param blue The blue component.
-	 * @throws IllegalArgumentException
-	 * 		If {@code red}, {@code green}, {@code blue}, or {@code alpha} are
-	 * 		outside the range 0 to 255.
-	 */
+	/** See {@link Color#Color(int, int, int)}. */
 	public VColor(final int red, final int green, final int blue) {
 		super(red, green, blue);
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param red The red component.
-	 * @param green The green component
-	 * @param blue The blue component.
-	 * @param alpha The alpha component.
-	 * @throws IllegalArgumentException
-	 * 		If {@code red}, {@code green}, {@code blue}, or {@code alpha} are
-	 * 		outside the range 0 to 255.
-	 */
+	/** See {@link Color#Color(int, int, int, int)}. */
 	public VColor(final int red, final int green, final int blue, final int alpha) {
 		super(red, green, blue, alpha);
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param rgb The combined RGB components.
-	 */
+	/** See {@link Color#Color(int)}. */
 	public VColor(final int rgb) {
 		super(rgb);
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param rgba The combined RGBA components.
-	 * @param hasAlpha {@code true} if the alpha bits are valid;
-	 *                 {@code false} otherwise.
-	 */
+	/** See {@link Color#Color(int, boolean)}. */
 	public VColor(final int rgba, final boolean hasAlpha) {
 		super(rgba, hasAlpha);
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param red The red component.
-	 * @param green The green component.
-	 * @param blue The blue component.
-	 * @throws IllegalArgumentException
-	 * 		If {@code red}, {@code green}, or {@code blue} are outside the range
-	 * 		0.0 to 1.0.
-	 */
+	/** See {@link Color#Color(float, float, float)}. */
 	public VColor(final float red, final float green, final float blue) {
 		super(red, green, blue);
 	}
 
-	/**
-	 * Constructs a new instance of {@code VColor}.
-	 *
-	 * @param red The red component.
-	 * @param green The green component.
-	 * @param blue The blue component.
-	 * @param alpha The alpha component.
-	 * @throws IllegalArgumentException
-	 * 		If {@code red}, {@code green}, {@code blue}, or {@code alpha} are
-	 * 		outside the range 0.0 to 1.0.
-	 */
+	/** See {@link Color#Color(float, float, float, float)}. */
 	public VColor(final float red, final float green, final float blue, final float alpha) {
 		super(red, green, blue, alpha);
 	}
 
 	/**
-	 * Constructs a new instance of {@code VColor}.
+	 * Constructs a new {@link VColor}.
 	 *
-	 * @param colorSpace
-	 * 		The {@code ColorSpace} to be used to interpret the components.
-	 * @param components
-	 * 		An arbitrary number of color components that are compatible with the
-	 * 		{@code ColorSpace}
+	 * @param colorSpace {@link ColorSpace} to be used to interpret the components.
+	 * @param components An arbitrary number of color components that are compatible with the {@code ColorSpace}.
 	 * @param alpha The alpha component.
 	 * @throws IllegalArgumentException
-	 * 		If any of the {@code components}, including the {@code alpha}
-	 * 		component, are outside the range 0.0 to 1.0.
+	 * 		If any of the {@code components}, including the {@code alpha} component, are outside the range 0.0 to 1.0.
 	 */
 	public VColor(final @NonNull ColorSpace colorSpace, final float[] components, final float alpha) {
 		super(colorSpace, components, alpha);
 	}
 
 	/**
-	 * Creates a shaded copy of this color, where a higher amount results in a
-	 * darker color.
+	 * Constructs a shaded copy of this {@link VColor}, where a higher amount results in a darker color.
 	 *
 	 * @param amount An amount to shade by.
-	 * @return The shaded color.
+	 * @return A new {@link VColor}, with the specified amount of shading applied.
 	 */
 	public VColor shade(final double amount) {
 		if (amount <= 0.0) {
@@ -134,11 +81,10 @@ public class VColor extends Color {
 	}
 
 	/**
-	 * Creates a tinted copy of this color, where a higher amount results in a
-	 * lighter color.
+	 * Creates a tinted copy of this {@link VColor}, where a higher amount results in a lighter color.
 	 *
 	 * @param amount An amount to tint by.
-	 * @return The shaded color.
+	 * @return A new {@link VColor}, with the specified amount of tinting applied.
 	 */
 	public VColor tint(final double amount) {
 		if (amount <= 0.0) {
