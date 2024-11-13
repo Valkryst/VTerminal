3ee597c923be25f45af22a42b0813948dd3ef152
Valkryst <github@valkryst.com>
2024-11-13 14:52:11 -0400

    Add JavaDoc to `VTextPane`

--- src/main/java/com/valkryst/VTerminal/component/VTextPane.java
+++ src/main/java/com/valkryst/VTerminal/component/VTextPane.java
@@ -6,11 +6,14 @@ import javax.swing.*;
 import javax.swing.text.StyledDocument;
 import java.awt.*;
 
+/** A {@link JTextPane} which uses the {@link VTerminalLookAndFeel} and its rendering hints. */
 public class VTextPane extends JTextPane {
+	/** See {@link JTextPane#JTextPane()}. */
 	public VTextPane() {
 		super();
 	}
 
+	/** See {@link JTextPane#JTextPane(StyledDocument)}. */
 	public VTextPane(final StyledDocument document) {
 		super(document);
 	}
