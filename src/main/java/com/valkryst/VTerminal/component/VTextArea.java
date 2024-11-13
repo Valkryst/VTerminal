ab39214474daf7f822f09804206ea4758db72f12
Valkryst <github@valkryst.com>
2024-11-13 14:58:23 -0400

    Add JavaDoc to `VTextArea`

--- src/main/java/com/valkryst/VTerminal/component/VTextArea.java
+++ src/main/java/com/valkryst/VTerminal/component/VTextArea.java
@@ -6,23 +6,29 @@ import javax.swing.*;
 import javax.swing.text.Document;
 import java.awt.*;
 
+/** A {@link JTextArea} which uses the {@link VTerminalLookAndFeel} and its rendering hints. */
 public class VTextArea extends JTextArea {
+	/** See {@link JTextArea#JTextArea(String)}. */
 	public VTextArea(final String text) {
 		super(text);
 	}
 
+	/** See {@link JTextArea#JTextArea(int, int)}. */
 	public VTextArea(final int rows, final int columns) {
 		super(rows, columns);
 	}
 
+	/** See {@link JTextArea#JTextArea(String, int, int)}. */
 	public VTextArea(final String text, final int rows, final int columns) {
 		super(text, rows, columns);
 	}
 
+	/** See {@link JTextArea#JTextArea(Document)}. */
 	public VTextArea(final Document document) {
 		super(document);
 	}
 
+	/** See {@link JTextArea#JTextArea(Document, String, int, int)}. */
 	public VTextArea(final Document document, final String text, final int rows, final int columns) {
 		super(document, text, rows, columns);
 	}
