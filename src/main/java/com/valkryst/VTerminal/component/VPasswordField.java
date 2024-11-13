87a432eab11c8cd329fdc3abf2f6b5e14ee524d2
Valkryst <github@valkryst.com>
2024-11-13 14:59:17 -0400

    Add JavaDoc to `VPasswordField`

--- src/main/java/com/valkryst/VTerminal/component/VPasswordField.java
+++ src/main/java/com/valkryst/VTerminal/component/VPasswordField.java
@@ -5,19 +5,24 @@ import com.valkryst.VTerminal.plaf.VTerminalLookAndFeel;
 import javax.swing.*;
 import java.awt.*;
 
+/** A {@link JPasswordField} which uses the {@link VTerminalLookAndFeel} and its rendering hints. */
 public class VPasswordField extends JPasswordField {
+	/** See {@link JPasswordField#JPasswordField()}. */
 	public VPasswordField() {
 		super();
 	}
 
+	/** See {@link JPasswordField#JPasswordField(String)}. */
 	public VPasswordField(final String text) {
 		super(text);
 	}
 
+	/** See {@link JPasswordField#JPasswordField(int)}. */
 	public VPasswordField(final int columns) {
 		super(columns);
 	}
 
+	/** See {@link JPasswordField#JPasswordField(String, int)}. */
 	public VPasswordField(final String text, final int columns) {
 		super(text, columns);
 	}
