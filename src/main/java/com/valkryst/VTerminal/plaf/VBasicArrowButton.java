d555a51fa4417d40a2b955ccfd198cc19d21dd47
Valkryst <github@valkryst.com>
2024-11-13 15:50:14 -0400

    Add JavaDoc to the constructor of `VBasicArrowButton`

--- src/main/java/com/valkryst/VTerminal/plaf/VBasicArrowButton.java
+++ src/main/java/com/valkryst/VTerminal/plaf/VBasicArrowButton.java
@@ -5,6 +5,11 @@ import javax.swing.plaf.basic.BasicArrowButton;
 import java.awt.*;
 
 public class VBasicArrowButton extends BasicArrowButton {
+	/**
+	 * Constructs a {@link VBasicArrowButton}.
+	 *
+	 * @param direction See {@link BasicArrowButton#BasicArrowButton(int)}.
+	 */
 	public VBasicArrowButton(final int direction) {
 		super(direction);
 		super.setBackground(UIManager.getColor("ScrollBar.background"));
