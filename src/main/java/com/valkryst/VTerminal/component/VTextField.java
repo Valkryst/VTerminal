3af2444947305ff73b434a9aaa7478abbcf94f92
Valkryst <github@valkryst.com>
2024-11-13 14:57:40 -0400

    Add JavaDoc to `VTextField`

--- src/main/java/com/valkryst/VTerminal/component/VTextField.java
+++ src/main/java/com/valkryst/VTerminal/component/VTextField.java
@@ -6,23 +6,29 @@ import javax.swing.*;
 import javax.swing.text.Document;
 import java.awt.*;
 
+/** A {@link JTextField} which uses the {@link VTerminalLookAndFeel} and its rendering hints. */
 public class VTextField extends JTextField {
+	/** See {@link JTextField#JTextField()}. */
 	public VTextField() {
 		super();
 	}
 
+	/** See {@link JTextField#JTextField(String)}. */
 	public VTextField(final String text) {
 		super(text);
 	}
 
+	/** See {@link JTextField#JTextField(int)}. */
 	public VTextField(final int columns) {
 		super(columns);
 	}
 
+	/** See {@link JTextField#JTextField(String, int)}. */
 	public VTextField(final String text, final int columns) {
 		super(text, columns);
 	}
 
+	/** See {@link JTextField#JTextField(Document, String, int)}. */
 	public VTextField(final Document document, final String text, final int column) {
 		super(document, text, column);
 	}
