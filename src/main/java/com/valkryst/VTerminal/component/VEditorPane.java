0f21bc62eebb0366db11a09570b67a620316c8af
Valkryst <github@valkryst.com>
2024-11-13 14:52:01 -0400

    Add JavaDoc to `VEditorPane`

--- src/main/java/com/valkryst/VTerminal/component/VEditorPane.java
+++ src/main/java/com/valkryst/VTerminal/component/VEditorPane.java
@@ -7,19 +7,24 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 
+/** A {@link JEditorPane} which uses the {@link VTerminalLookAndFeel} and its rendering hints. */
 public class VEditorPane extends JEditorPane {
+	/** See {@link JEditorPane#JEditorPane()}. */
 	public VEditorPane() {
 		super();
 	}
 
+	/** See {@link JEditorPane#JEditorPane(URL)}. */
 	public VEditorPane(final URL initialPage) throws IOException {
 		super(initialPage);
 	}
 
+	/** See {@link JEditorPane#JEditorPane(String)}. */
 	public VEditorPane(final String url) throws IOException {
 		super(url);
 	}
 
+	/** See {@link JEditorPane#JEditorPane(String, String)}. */
 	public VEditorPane(final String type, final String text) {
 		super(type, text);
 	}
